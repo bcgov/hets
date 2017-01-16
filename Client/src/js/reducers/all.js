82b1c32874ef9620447074b486789cd0dc24786d
georgelwalker <gwalker@escapesystems.com>
2017-01-16 15:42:17 -0800

    HETS-19 updated client file

--- Client/src/js/reducers/all.js
+++ Client/src/js/reducers/all.js
@@ -2,15 +2,13 @@ import { combineReducers } from 'redux';
 
 import uiReducer from './ui';
 import userReducer from './user';
-import searchReducer from './search';
 import modelsReducer from './models';
 import lookupsReducer from './lookups';
 
 
 export default combineReducers({
   ui      : uiReducer,
-  user    : userReducer,
-  search  : searchReducer,
+  user    : userReducer,  
   models  : modelsReducer,
   lookups : lookupsReducer,
 });
