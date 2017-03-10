5c26cf867f4c92512efcc5958dabf743a6d5bfbe
Alejandro Sanchez <emailforasr@gmail.com>
2017-03-09 22:53:56 -0800

    HETS-172 Fix minor bug in add owner popup

--- Client/src/js/views/dialogs/OwnersAddDialog.jsx
+++ Client/src/js/views/dialogs/OwnersAddDialog.jsx
@@ -58,7 +58,7 @@ var OwnersAddDialog = React.createClass({
   },
 
   didChange() {
-    return notBlank(this.state.name) && notBlank(this.state.equipmentPrefix);
+    return notBlank(this.state.name) && notBlank(this.state.equipmentPrefix) && this.state.meetsResidency !== false && this.state.localAreaId !== 0;
   },
 
   isValid() {
