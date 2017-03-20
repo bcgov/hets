8e76499061ead2f08156d055403d0b30004ad854
georgelwalker <gwalker@escapesystems.com>
2017-03-20 15:25:43 -0700

    HETS-162 - Seniority List Calculation

---
+++ Server/src/HETSAPI/Models.Impl/EquipmentType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HETSAPI.Models
+{
+    public partial class EquipmentType
+    {
+        public const int DUMP_TRUCK_BLOCKS = 3;
+        public const int OTHER_BLOCKS = 2;
+        public const int PRIMARY_BLOCK = 1;
+        public const int SECONDARY_BLOCK = 2;
+        public const int OPEN_BLOCK_NON_DUMP_TRUCK = 2;
+        public const int OPEN_BLOCK_DUMP_TRUCK = 3;
+    }
+}
