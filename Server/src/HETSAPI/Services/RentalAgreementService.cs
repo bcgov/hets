14dc703a51b2ef4d3dfc0fb09a490f8df79264ba
georgelwalker <gwalker@escapesystems.com>
2017-03-27 14:02:00 -0700

    HETS-246 - Add PDF service to rental agreements

--- Server/src/HETSAPI/Services/RentalAgreementService.cs
+++ Server/src/HETSAPI/Services/RentalAgreementService.cs
@@ -61,6 +61,14 @@ namespace HETSAPI.Services
         /// <summary>
         /// 
         /// </summary>
+        /// <remarks>Returns a PDF version of the specified rental agreement</remarks>
+        /// <param name="id">id of RentalAgreement to obtain the PDF for</param>
+        /// <response code="200">OK</response>
+        IActionResult RentalagreementsIdPdfGetAsync(int id);
+
+        /// <summary>
+        /// 
+        /// </summary>
         /// <param name="id">id of RentalAgreement to fetch</param>
         /// <param name="item"></param>
         /// <response code="200">OK</response>
---
+++ Server/src/HETSAPI/ViewModels/RentalAgreementPdfViewModel.cs
@@ -0,0 +1,170 @@
+/*
+ * REST API Documentation for the MOTI Hired Equipment Tracking System (HETS) Application
+ *
+ * The Hired Equipment Program is for owners/operators who have a dump truck, bulldozer, backhoe or  other piece of equipment they want to hire out to the transportation ministry for day labour and  emergency projects.  The Hired Equipment Program distributes available work to local equipment owners. The program is  based on seniority and is designed to deliver work to registered users fairly and efficiently  through the development of local area call-out lists. 
+ *
+ * OpenAPI spec version: v1
+ * 
+ * 
+ */
+
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using HETSAPI.Models;
+
+namespace HETSAPI.ViewModels
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [DataContract]
+    public partial class RentalAgreementPdfViewModel : IEquatable<RentalAgreementPdfViewModel>
+    {
+        /// <summary>
+        /// Default constructor, required by entity framework
+        /// </summary>
+        public RentalAgreementPdfViewModel()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RentalAgreementPdfViewModel" /> class.
+        /// </summary>
+        /// <param name="Id">Id (required).</param>
+        /// <param name="Number">A system-generated unique rental agreement number in a format defined by the business as suitable for the business and client to see and use..</param>
+        public RentalAgreementPdfViewModel(int Id, string Number = null)
+        {   
+            this.Id = Id;
+            this.Number = Number;
+        }
+
+        /// <summary>
+        /// Gets or Sets Id
+        /// </summary>
+        [DataMember(Name="id")]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// A system-generated unique rental agreement number in a format defined by the business as suitable for the business and client to see and use.
+        /// </summary>
+        /// <value>A system-generated unique rental agreement number in a format defined by the business as suitable for the business and client to see and use.</value>
+        [DataMember(Name="number")]
+        [MetaDataExtension (Description = "A system-generated unique rental agreement number in a format defined by the business as suitable for the business and client to see and use.")]
+        public string Number { get; set; }
+
+        /// <summary>
+        /// Returns the string presentation of the object
+        /// </summary>
+        /// <returns>String presentation of the object</returns>
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append("class RentalAgreementPdfViewModel {\n");
+            sb.Append("  Id: ").Append(Id).Append("\n");
+            sb.Append("  Number: ").Append(Number).Append("\n");
+            sb.Append("}\n");
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Returns the JSON string presentation of the object
+        /// </summary>
+        /// <returns>JSON string presentation of the object</returns>
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+
+        /// <summary>
+        /// Returns true if objects are equal
+        /// </summary>
+        /// <param name="obj">Object to be compared</param>
+        /// <returns>Boolean</returns>
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) { return false; }
+            if (ReferenceEquals(this, obj)) { return true; }
+            if (obj.GetType() != GetType()) { return false; }
+            return Equals((RentalAgreementPdfViewModel)obj);
+        }
+
+        /// <summary>
+        /// Returns true if RentalAgreementPdfViewModel instances are equal
+        /// </summary>
+        /// <param name="other">Instance of RentalAgreementPdfViewModel to be compared</param>
+        /// <returns>Boolean</returns>
+        public bool Equals(RentalAgreementPdfViewModel other)
+        {
+
+            if (ReferenceEquals(null, other)) { return false; }
+            if (ReferenceEquals(this, other)) { return true; }
+
+            return                 
+                (
+                    this.Id == other.Id ||
+                    this.Id.Equals(other.Id)
+                ) &&                 
+                (
+                    this.Number == other.Number ||
+                    this.Number != null &&
+                    this.Number.Equals(other.Number)
+                );
+        }
+
+        /// <summary>
+        /// Gets the hash code
+        /// </summary>
+        /// <returns>Hash code</returns>
+        public override int GetHashCode()
+        {
+            // credit: http://stackoverflow.com/a/263416/677735
+            unchecked // Overflow is fine, just wrap
+            {
+                int hash = 41;
+                // Suitable nullity checks
+                                   
+                hash = hash * 59 + this.Id.GetHashCode();                if (this.Number != null)
+                {
+                    hash = hash * 59 + this.Number.GetHashCode();
+                }                
+                
+                return hash;
+            }
+        }
+
+        #region Operators
+        
+        /// <summary>
+        /// Equals
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static bool operator ==(RentalAgreementPdfViewModel left, RentalAgreementPdfViewModel right)
+        {
+            return Equals(left, right);
+        }
+
+        /// <summary>
+        /// Not Equals
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static bool operator !=(RentalAgreementPdfViewModel left, RentalAgreementPdfViewModel right)
+        {
+            return !Equals(left, right);
+        }
+
+        #endregion Operators
+    }
+}
