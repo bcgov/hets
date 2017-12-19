aceb1598101d40b74ccdda7c75e4b56a4903dd33
GeorgeWalker <george@walkersoftware.ca>
2017-12-18 21:23:59 -0800

    PDF service improvements

--- PDF/src/PDF.Server/Controllers/PDFController.cs
+++ PDF/src/PDF.Server/Controllers/PDFController.cs
@@ -1,78 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-
-using Microsoft.EntityFrameworkCore;
-using PDF.Models;
-using Microsoft.AspNetCore.Http;
-using Newtonsoft.Json;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.NodeServices;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
 
 namespace PDF.Controllers
 {
+    public class PDFRequest
+    {
+        public string html { get; set; }
+        public string options { get; set; }
+    }
     public class JSONResponse
     {
         public string type;
         public byte[] data;
     }
-    [Route("api/[controller]")]
+    [Route("api/[controller]")] 
     public class PDFController : Controller
     {
-        private readonly IConfiguration Configuration;
-        private readonly DbAppContext _context;
-        private readonly IHttpContextAccessor _httpContextAccessor;
-
-        private string userId;
-        private string guid;
-        private string directory;
-
         protected ILogger _logger;
 
-        public PDFController(IHttpContextAccessor httpContextAccessor, IConfigurationRoot configuration, DbAppContext context, ILoggerFactory loggerFactory)
+        public PDFController(ILoggerFactory loggerFactory)
         {
-            Configuration = configuration;
-
-            _httpContextAccessor = httpContextAccessor;
-            _context = context;
-
-            userId = getFromHeaders("SM_UNIVERSALID");
-            guid = getFromHeaders("SMGOV_USERGUID");
-            directory = getFromHeaders("SM_AUTHDIRNAME");
-
             _logger = loggerFactory.CreateLogger(typeof(PDFController));
         }
 
-        private string getFromHeaders(string key)
-        {
-            string result = null;
-            if (Request.Headers.ContainsKey(key))
-            {
-                result = Request.Headers[key];
-            }
-            return result;
-        }
 
         [HttpPost]
-        [Route("GetPDF")]
+        [Route("BuildPDF")]
 
-        public async Task<IActionResult> GetPDF([FromServices] INodeServices nodeServices, [FromBody]  Object rawdata )
+        public async Task<IActionResult> BuildPDF([FromServices] INodeServices nodeServices, [FromBody]  PDFRequest rawdata )
         {
+            JObject options = JObject.Parse(rawdata.options);
             JSONResponse result = null;
-            var options = new { format="letter", orientation= "landscape" };
+            //var options = new { format="letter", orientation= "portrait" }; 
 
-            // execute the Node.js component
-            result = await nodeServices.InvokeAsync<JSONResponse>("./pdf", "rental_agreement", rawdata, options);
+            // execute the Node.js component to generate a PDF
+            result = await nodeServices.InvokeAsync<JSONResponse>("./pdf.js", rawdata.html, options);
+            options = null;
 
             return new FileContentResult(result.data, "application/pdf");
+
+            _logger.LogInformation("Rendered document.");
         }
 
-        protected HttpRequest Request
-        {
-            get { return _httpContextAccessor.HttpContext.Request; }
-        }
     }
 }
