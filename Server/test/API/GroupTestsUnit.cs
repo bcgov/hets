77106d01db811f6c6fac903964d67db191aad68c
Paul Litton <paul.m.litton@gmail.com>
2018-01-07 13:53:38 -0800

    Error starting frontend

--- Server/test/API/GroupTestsUnit.cs
+++ Server/test/API/GroupTestsUnit.cs
@@ -1,5 +1,7 @@
 using Xunit;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 using Moq;
 using HETSAPI.Models;
 using HETSAPI.Controllers;
@@ -18,7 +20,10 @@ namespace HETSAPI.Test
 		{
             DbContextOptions<DbAppContext> options = new DbContextOptions<DbAppContext>();
             Mock<DbAppContext> dbAppContext = new Mock<DbAppContext>(null, options);
-            GroupService _service = new GroupService(dbAppContext.Object);
+
+            ILoggerFactory loggerFactory = new LoggerFactory();
+
+            GroupService _service = new GroupService(dbAppContext.Object, loggerFactory);
             _GroupApi = new GroupController (_service);
 		}
 		
