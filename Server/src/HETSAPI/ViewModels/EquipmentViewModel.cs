d5901b5eaa5e41dbd5b9ae9f4ce4f418132d517d
Paul Litton <paul.m.litton@gmail.com>
2018-01-15 11:12:57 -0800

    HETS-431 - Fix sort order on the Equipment Results Screen - Seniority List

--- Server/src/HETSAPI/ViewModels/EquipmentViewModel.cs
+++ Server/src/HETSAPI/ViewModels/EquipmentViewModel.cs
@@ -130,6 +130,18 @@ namespace HETSAPI.ViewModels
             DuplicateEquipment = duplicateEquipment;
             IsWorking = isWorking;
             LastTimeRecordDateThisYear = lastTimeRecordDateThisYear;
+
+            // calculate "seniority sort order" & round the seniority value (3 decimal places)
+            if (Seniority != null && Seniority > 0)
+            {
+                Seniority = (float)Math.Round((Decimal)seniority, 3, MidpointRounding.AwayFromZero);
+
+                if (BlockNumber != null)
+                {
+                    float temp = (float)BlockNumber * 10;
+                    SenioritySortOrder = temp + Seniority;
+                }
+            }
         }
 
         /// <summary>
@@ -471,6 +483,11 @@ namespace HETSAPI.ViewModels
         /// </summary>
         [DataMember(Name="lastTimeRecordDateThisYear")]
         public DateTime? LastTimeRecordDateThisYear { get; set; }
+
+        /// <summary>
+        /// Used to sort the Equipment by Seniority in the UI
+        /// </summary>
+        public float? SenioritySortOrder { get; set; }
 
         /// <summary>
         /// Returns the string presentation of the object
