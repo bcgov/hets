8e494aee4493e3d749376328e17d7f72855ec473
Paul Litton <paul.m.litton@gmail.com>
2018-01-22 14:39:40 -0800

    HETS-341 - Time Entry Screen for hired equipment - Back End Added a new "bulk" time entry service under project (allows a user to enter multiple records)

--- Server/src/HETSAPI/Services/ProjectService.cs
+++ Server/src/HETSAPI/Services/ProjectService.cs
@@ -71,7 +71,7 @@ namespace HETSAPI.Services
         /// <remarks>Gets an Project&#39;s Time Records</remarks>
         /// <param name="id">id of Project to fetch Time Records for</param>
         /// <response code="200">OK</response>
-        IActionResult ProjectsIdTimeRecordsGetAsync([FromRoute] int id);
+        IActionResult ProjectsIdTimeRecordsGetAsync(int id);
 
         /// <summary>
         /// Add a time record to a project
@@ -80,7 +80,16 @@ namespace HETSAPI.Services
         /// <param name="id">id of Project to add a time record for</param>
         /// <param name="item">Adds to Project Time Record</param>
         /// <response code="200">OK</response>
-        IActionResult ProjectsIdTimeRecordsPostAsync([FromRoute] int id, [FromBody] TimeRecord item);
+        IActionResult ProjectsIdTimeRecordsPostAsync(int id, TimeRecord item);
+
+        /// <summary>
+        /// Update or create an array of time records associated with a project
+        /// </summary>
+        /// <remarks>Update a Project&#39;s Time Records</remarks>
+        /// <param name="id">id of Project to update Time Records for</param>
+        /// <param name="items">Array of Project Time Records</param>
+        /// <response code="200">OK</response>
+        IActionResult ProjectsIdTimeRecordsBulkPostAsync(int id, TimeRecord[] items);
 
         /// <summary>
         /// Get equipment associated with a project
