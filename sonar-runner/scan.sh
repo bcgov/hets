6f4c6c417045f5b407e23f42cb9cc90f3c6af31d
Roland Stens <roland.stens@gmail.com>
2018-01-24 15:33:07 -0800

    Sonar-Runner files added

--- sonar-runner/scan.sh
+++ sonar-runner/scan.sh
@@ -1,6 +1,5 @@
 cd ..
-cd Server
-mono /usr/lib/sonar-scanner/SonarQube.Scanner.MSBuild.exe begin /d:sonar.host.url=http://sonarqube-agehlers-sandbox.pathfinder.gov.bc.ca /d:sonar.host.port=80 /k:HETSSERVER
+mono /usr/lib/sonar-scanner/SonarQube.Scanner.MSBuild.exe begin /s:${PWD}/SonarQube.Analysis.xml /d:sonar.url=${SONARQUBE_URL} /k:"org.sonarqube:bcgov-hets-all-pipeline" /n:"HETS - Pipeline" /v:"1.$BUILD_NUMBER"
 dotnet restore
 dotnet build
 dotnet test
