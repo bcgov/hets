c371fb3a89f056c5071cf5a07ba0bbd6659de744
Roland Stens <roland.stens@gmail.com>
2018-01-26 13:34:26 -0800

    Refinements for the sonar scanner in the pipeline

--- sonar-runner/scan.sh
+++ sonar-runner/scan.sh
@@ -1,6 +1,7 @@
 PATHXML=$(pwd -P)
+DT=$(TZ='America/Vancouver' date +%Y%m%d)
 cd ..
-mono /usr/lib/sonar-scanner/SonarQube.Scanner.MSBuild.exe begin /s:$PATHXML/SonarQube.Analysis.xml /d:sonar.url=http://sonarqube:9000 /k:"org.sonarqube:bcgov-hets-all-pipeline" /n:"HETS - Pipeline" /v:"1.$BUILD_NUMBER"
+mono /usr/lib/sonar-scanner/SonarQube.Scanner.MSBuild.exe begin /s:$PATHXML/SonarQube.Analysis.xml /n:"HETS - Pipeline DOTNET" /v:"$DT.$BUILD_NUMBER"
 dotnet restore
 dotnet build
 dotnet test
