33408cfbd26ac00aac797f744e87e31c6e90ac2e
Paul Litton <paul.m.litton@gmail.com>
2018-01-29 14:14:41 -0800

    HETS-450 - Equipment looses District and Local Area information when hired

--- Server/src/HETSAPI/Services.Impl/RentalRequestService.cs
+++ Server/src/HETSAPI/Services.Impl/RentalRequestService.cs
@@ -465,7 +465,7 @@ namespace HETSAPI.Services.Impl
             if (exists)
             {
                 // check that we have a rotation list
-                RentalRequest result = _context.RentalRequests
+                RentalRequest result = _context.RentalRequests.AsNoTracking()
                     .Include(x => x.DistrictEquipmentType)
                         .ThenInclude(y => y.EquipmentType)
                     .Include(x => x.FirstOnRotationList)
@@ -473,6 +473,8 @@ namespace HETSAPI.Services.Impl
                     .Include(x => x.RentalRequestRotationList)
                         .ThenInclude(y => y.Equipment)
                             .ThenInclude(r => r.EquipmentAttachments)
+                        .ThenInclude(y => y.Equipment)
+                            .ThenInclude(r => r.LocalArea)
                     .Include(x => x.RentalRequestRotationList)
                         .ThenInclude(y => y.Equipment)
                             .ThenInclude(e => e.Owner)
