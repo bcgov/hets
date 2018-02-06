e45380a63087e45b30824dc6e7084a391267fbb6
Paul Litton <paul.m.litton@gmail.com>
2018-02-06 13:01:26 -0800

    New api to return owners for a specific district

--- Server/src/HETSAPI/Services/DistrictService.cs
+++ Server/src/HETSAPI/Services/DistrictService.cs
@@ -60,5 +60,11 @@ namespace HETSAPI.Services
         /// <param name="item"></param>
         /// <response code="201">District created</response>
         IActionResult DistrictsPostAsync(District item);
+
+        /// <summary>
+        /// Get all owners by district (minimal data returned) - lookup
+        /// </summary>
+        /// <response code="200">OK</response>
+        IActionResult DistrictOwnersGetAsync(int id);
     }
 }
