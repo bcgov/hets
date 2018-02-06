a3db8b9c20ff1eb7f28aeb8101535dfecbb6b946
Paul Litton <paul.m.litton@gmail.com>
2018-02-06 15:01:10 -0800

    HETS-476 - Frontend-Need the ability to cancel a Rental request

--- Server/src/HETSAPI/Services/RentalAgreementService.cs
+++ Server/src/HETSAPI/Services/RentalAgreementService.cs
@@ -93,5 +93,57 @@ namespace HETSAPI.Services
         /// <param name="items">Array of Rental Agreement Time Records</param>
         /// <response code="200">OK</response>
         IActionResult RentalAgreementsIdTimeRecordsBulkPostAsync(int id, TimeRecord[] items);
+
+        /// <summary>
+        /// Get rate records for a rental agreement
+        /// </summary>
+        /// <remarks>Gets a Rental Agreement&#39;s Time Records</remarks>
+        /// <param name="id">id of Rental Agreement to fetch Time Records for</param>
+        /// <response code="200">OK</response>
+        IActionResult RentalAgreementsIdRentalAgreementRatesGetAsync(int id);
+
+        /// <summary>
+        /// Add a rate record to a rental agreement
+        /// </summary>
+        /// <remarks>Adds Rental Agreement Rate Record</remarks>
+        /// <param name="id">id of Rental Agreement to add a rate record for</param>
+        /// <param name="item">Adds to Rental Agreement Rate Record</param>
+        /// <response code="200">OK</response>
+        IActionResult RentalAgreementsIdRentalAgreementRatesPostAsync(int id, RentalAgreementRate item);
+
+        /// <summary>
+        /// Update or create an array of rate records associated with a rental agreement
+        /// </summary>
+        /// <remarks>Update a Rental Agreement&#39;s Time Records</remarks>
+        /// <param name="id">id of Rental Agreement to update Time Records for</param>
+        /// <param name="items">Array of Rental Agreement Time Records</param>
+        /// <response code="200">OK</response>
+        IActionResult RentalAgreementsIdRentalAgreementRatesBulkPostAsync(int id, RentalAgreementRate[] items);
+
+        /// <summary>
+        /// Get condition records for a rental agreement
+        /// </summary>
+        /// <remarks>Gets a Rental Agreement&#39;s Time Records</remarks>
+        /// <param name="id">id of Rental Agreement to fetch Time Records for</param>
+        /// <response code="200">OK</response>
+        IActionResult RentalAgreementsIdRentalAgreementConditionsGetAsync(int id);
+
+        /// <summary>
+        /// Add a condition record to a rental agreement
+        /// </summary>
+        /// <remarks>Adds Rental Agreement Condition Record</remarks>
+        /// <param name="id">id of Rental Agreement to add a condition record for</param>
+        /// <param name="item">Adds to Rental Agreement Condition Record</param>
+        /// <response code="200">OK</response>
+        IActionResult RentalAgreementsIdRentalAgreementConditionsPostAsync(int id, RentalAgreementCondition item);
+
+        /// <summary>
+        /// Update or create an array of condition records associated with a rental agreement
+        /// </summary>
+        /// <remarks>Update a Rental Agreement&#39;s Time Records</remarks>
+        /// <param name="id">id of Rental Agreement to update Time Records for</param>
+        /// <param name="items">Array of Rental Agreement Time Records</param>
+        /// <response code="200">OK</response>
+        IActionResult RentalAgreementsIdRentalAgreementConditionsBulkPostAsync(int id, RentalAgreementCondition[] items);
     }
 }
