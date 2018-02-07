733c746397b414b22b2f428b48943a3d5373dece
Kieran <kieran_summerfeldt@hotmail.com>
2018-02-07 09:17:29 -0800

    HETS-487. Multi inputs WIP

--- Client/src/js/views/dialogs/AttachmentRatesEditDialog.jsx
+++ Client/src/js/views/dialogs/AttachmentRatesEditDialog.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Grid, Row, Col } from 'react-bootstrap';
-import { Form, FormGroup, HelpBlock, ControlLabel, FormControl } from 'react-bootstrap';
+import { Form, FormGroup, HelpBlock, ControlLabel, FormControl, Button, Glyphicon } from 'react-bootstrap';
 
 import _ from 'lodash';
 
@@ -39,6 +39,19 @@ var AttachmentRatesEditDialog = React.createClass({
       comment: attachmentRate.comment || '',
       includeInTotal: attachmentRate.isIncludedInTotal || false,
       ratePeriod: rentalAgreement.ratePeriod,  // The period for attachments is the same as for the Pay Rate so is there, but not displayed.
+      numberOfInputs: 1,
+      forms: {
+        1: {
+          componentName: attachmentRate.componentName || '',
+          comment: attachmentRate.comment || '',
+          rate: attachmentRate.rate || 0.0,
+          percentOfEquipmentRate: attachmentRate.percentOfEquipmentRate || 0,
+          percentOrRateOption: isNew || this.props.attachmentRate.percentOfEquipmentRate > 0 ? PERCENT_RATE : DOLLAR_RATE,
+          percentOrRateValue: isNew ? 10 : this.props.attachmentRate.rate || this.props.attachmentRate.percentOfEquipmentRate || 0,
+          commentError: '',
+          componentNameError: '',
+        },
+      },
 
       ui : {
         percentOrRateOption: isNew || this.props.attachmentRate.percentOfEquipmentRate > 0 ? PERCENT_RATE : DOLLAR_RATE,
@@ -54,58 +67,107 @@ var AttachmentRatesEditDialog = React.createClass({
   componentDidMount() {
   },
 
-  updateState(state, callback) {
-    this.setState(state, callback);
+  updateState(value) {
+    // this.setState(state, callback);
+    let property = Object.keys(value)[0];
+    let stateValue = Object.values(value)[0];
+    let number = property.match(/\d+/g)[0];
+    let stateName = property.match(/[a-zA-Z]+/g)[0];
+    let state = { [stateName]:  stateValue };
+    let updatedState = { ...this.state.forms, [number]: { ...this.state.forms[number], ...state } };
+    this.setState({ forms: updatedState });
   },
 
-  updateUIState(state, callback) {
-    var nextState = { ui: { ...this.state.ui, ...state } };
+  updateUIState(value) {
+    // var nextState = { ui: { ...this.state.ui, ...state } };
 
-    // Update rate and percentOfEquipmentRate fields from what has been entered in the form
-    var option = nextState.ui.percentOrRateOption;
-    var value = nextState.ui.percentOrRateValue;
+    // // Update rate and percentOfEquipmentRate fields from what has been entered in the form
+    // var option = nextState.ui.percentOrRateOption;
+    // var value = nextState.ui.percentOrRateValue;
 
-    this.setState({ ...nextState, ...{
-      rate: option == DOLLAR_RATE ? value : 0,
-      percentOfEquipmentRate: option == PERCENT_RATE ? value : 0,
-    }}, callback);
+    // this.setState({ ...nextState, ...{
+    //   rate: option == DOLLAR_RATE ? value : 0,
+    //   percentOfEquipmentRate: option == PERCENT_RATE ? value : 0,
+    // }}, callback);
+    let property = Object.keys(value)[0];
+    let stateValue = Object.values(value)[0];
+    let number = property.match(/\d+/g)[0];
+    let stateName = property.match(/[a-zA-Z]+/g)[0];
+    let state = { [stateName]:  stateValue };
+    let updatedState = { ...this.state.forms, [number]: { ...this.state.forms[number], ...state } };
+    this.setState({ forms: updatedState });
   },
 
   didChange() {
-    if (this.state.componentName !== this.props.attachmentRate.componentName) { return true; }
-    if (this.state.rate !== this.props.attachmentRate.rate) { return true; }
-    if (this.state.includeInTotal !== this.props.attachmentRate.isIncludedInTotal) { return true; }
-    if (this.state.percentOfEquipmentRate !== this.props.attachmentRate.percentOfEquipmentRate) { return true; }
-    if (this.state.comment !== this.props.attachmentRate.comment) { return true; }
+    // if (this.state.componentName !== this.props.attachmentRate.componentName) { return true; }
+    // if (this.state.rate !== this.props.attachmentRate.rate) { return true; }
+    // if (this.state.includeInTotal !== this.props.attachmentRate.isIncludedInTotal) { return true; }
+    // if (this.state.percentOfEquipmentRate !== this.props.attachmentRate.percentOfEquipmentRate) { return true; }
+    // if (this.state.comment !== this.props.attachmentRate.comment) { return true; }
 
-    return false;
+    // return false;
+    return true;
   },
 
   isValid() {
-    this.setState({
-      componentNameError: '',
-      rateError: '',
+    // this.setState({
+    //   componentNameError: '',
+    //   rateError: '',
+    // });
+
+    // var valid = true;
+
+    // if (isBlank(this.state.componentName)) {
+    //   this.setState({ componentNameError: 'Rate type is required' });
+    //   valid = false;
+    // }
+
+    // if (this.state.componentName === EQUIPMENT_ATTACHMENT_OTHER && isBlank(this.state.comment)) {
+    //   this.setState({ commentError: 'Comment is required '});
+    //   valid = false;
+    // }
+
+    // if (isBlank(this.state.ui.percentOrRateValue) ) {
+    //   this.setState({ rateError: 'Pay rate is required' });
+    //   valid = false;
+    // } else if (this.state.ui.percentOrRateValue < 1) {
+    //   this.setState({ rateError: 'Pay rate not valid' });
+    //   valid = false;
+    // }
+
+    // return valid;
+    let forms = { ...this.state.forms };
+
+    let formsResetObj = forms;
+    Object.keys(forms).map((key) => {
+      let state = { ...forms[key], componentNameError: '', commentError: '' };
+      formsResetObj[key] = state;
     });
+    
+    this.setState({ forms: formsResetObj });
+    let valid = true;
 
-    var valid = true;
-
-    if (isBlank(this.state.componentName)) {
-      this.setState({ componentNameError: 'Rate type is required' });
-      valid = false;
-    }
-
-    if (this.state.componentName === EQUIPMENT_ATTACHMENT_OTHER && isBlank(this.state.comment)) {
-      this.setState({ commentError: 'Comment is required '});
-      valid = false;
-    }
-
-    if (isBlank(this.state.ui.percentOrRateValue) ) {
-      this.setState({ rateError: 'Pay rate is required' });
-      valid = false;
-    } else if (this.state.ui.percentOrRateValue < 1) {
-      this.setState({ rateError: 'Pay rate not valid' });
-      valid = false;
-    }
+    let formsErrorsObj = forms;
+    Object.keys(forms).map((key) => {
+      if (forms[key].componentName === EQUIPMENT_ATTACHMENT_OTHER && isBlank(forms[key].comment)) {
+        let state = { ...forms[key], commentError: 'Comment is required.' };
+        formsErrorsObj[key] = state;
+        valid = false;
+      }
+      if (isBlank(forms[key].componentName)) {
+        let state = { ...forms[key], componentNameError: 'Rate type is required.' };
+        formsErrorsObj[key] = state;
+        valid = false;
+      }
+      // if (isBlank(this.state.ui.percentOrRateValue) ) {
+      //   this.setState({ rateError: 'Pay rate is required' });
+      //   valid = false;
+      // } else if (this.state.ui.percentOrRateValue < 1) {
+      //   this.setState({ rateError: 'Pay rate not valid' });
+      //   valid = false;
+      // }
+    });
+    this.setState({ forms: formsErrorsObj });
 
     return valid;
   },
@@ -132,6 +194,32 @@ var AttachmentRatesEditDialog = React.createClass({
     return null;
   },
 
+  addInput() {
+    if (this.state.numberOfInputs < 10) {
+      let numberOfInputs = Object.keys(this.state.forms).length;
+      this.setState({ 
+        numberOfInputs: this.state.numberOfInputs + 1,
+        forms: { 
+          ...this.state.forms, 
+          [numberOfInputs + 1]: { 
+          }, 
+        },
+      });
+    }
+  },
+
+  removeInput() {
+    if (this.state.numberOfInputs > 1) {
+      let numberOfInputs = Object.keys(this.state.forms).length;
+      let forms = { ...this.state.forms };
+      delete forms[numberOfInputs];
+      this.setState({ 
+        numberOfInputs: this.state.numberOfInputs - 1,
+        forms: forms, 
+      });
+    }
+  },
+
   render() {
     var attachmentRate = this.props.attachmentRate;
     var rentalAgreement = this.props.rentalAgreement;
@@ -144,57 +232,79 @@ var AttachmentRatesEditDialog = React.createClass({
       title={
         <strong>Rental Agreement - Attachments</strong>
       }>
-      <Form>
-        <Grid fluid>
-          <Row>
-            <Col md={6}>
-              <FormGroup controlId="componentName" validationState={ this.state.componentNameError ? 'error' : null }>
-                <ControlLabel>Rate Component <sup>*</sup></ControlLabel>
-                {/*TODO - use lookup list*/}
-                <DropdownControl id="componentName" disabled={ isReadOnly } updateState={ this.updateState }
-                  items={ attachments } title={ this.state.componentName } className="full-width" />
-                <HelpBlock>{ this.state.componentNameError }</HelpBlock>
-              </FormGroup>
-            </Col>
-            <Col md={2}>
-              <FormGroup controlId="percentOrRateValue" validationState={ this.state.rateError ? 'error' : null }>
-                <ControlLabel>Rate <sup>*</sup></ControlLabel>
-                <FormInputControl type="float" min={ 0 } defaultValue={ this.state.ui.percentOrRateValue } readOnly={ isReadOnly } updateState={ this.updateUIState } inputRef={ ref => { this.input = ref; }}/>
-                <HelpBlock>{ this.state.rateError }</HelpBlock>
-              </FormGroup>
-            </Col>
-            <Col md={2}>
-              <FormGroup controlId="percentOrRateOption">
-                <ControlLabel>&nbsp;</ControlLabel>
-                <DropdownControl id="percentOrRateOption" disabled={ isReadOnly } title={ this.state.ui.percentOrRateOption } updateState={ this.updateUIState }
-                  items={[ PERCENT_RATE, DOLLAR_RATE ]} />
-              </FormGroup>
-            </Col>
-            <Col md={2}>
-              <FormGroup>
-                <ControlLabel>&nbsp;</ControlLabel>
-                <FormControl.Static id="dollar-value" title={ formatCurrency(this.dollarValue()) }>{ formatCurrency(this.dollarValue()) }</FormControl.Static>
-              </FormGroup>
-            </Col>
-          </Row>
-          <Row>
-            <Col md={12}>
-              <FormGroup controlId="comment" validationState={ this.state.commentError ? 'error' : null }>
-                <ControlLabel>Comment</ControlLabel>
-                <FormInputControl componentClass="textarea" defaultValue={ this.state.comment } readOnly={ isReadOnly } updateState={ this.updateState } />
-                <HelpBlock>{ this.state.commentError }</HelpBlock>
-              </FormGroup>
-            </Col>
-          </Row>
-          <Row>
-            <Col md={12}>
-              <FormGroup controlId="includeInTotal">
-                <CheckboxControl id="includeInTotal" checked={ this.state.includeInTotal } updateState={ this.updateState }>Include in total</CheckboxControl>
-              </FormGroup>
-            </Col>
-          </Row>
-        </Grid>
-      </Form>
+      { Object.keys(this.state.forms).map(key => {
+        return (
+        <Form key={key}>
+          <Grid fluid>
+            <Row>
+              <Col md={6}>
+                <FormGroup controlId="componentName" validationState={ this.state.forms[key].componentNameError ? 'error' : null }>
+                  <ControlLabel>Rate Component <sup>*</sup></ControlLabel>
+                  {/*TODO - use lookup list*/}
+                  <DropdownControl id={`componentName${key}`} disabled={ isReadOnly } updateState={ this.updateState }
+                    items={ attachments } title={ this.state.forms[key].componentName } className="full-width" />
+                  <HelpBlock>{ this.state.forms[key].componentNameError }</HelpBlock>
+                </FormGroup>
+              </Col>
+              <Col md={2}>
+                <FormGroup controlId="percentOrRateValue" validationState={ this.state.rateError ? 'error' : null }>
+                  <ControlLabel>Rate <sup>*</sup></ControlLabel>
+                  <FormInputControl type="float" min={ 0 } defaultValue={ this.state.ui.percentOrRateValue } readOnly={ isReadOnly } updateState={ this.updateUIState } inputRef={ ref => { this.input = ref; }}/>
+                  <HelpBlock>{ this.state.rateError }</HelpBlock>
+                </FormGroup>
+              </Col>
+              <Col md={2}>
+                <FormGroup controlId="percentOrRateOption">
+                  <ControlLabel>&nbsp;</ControlLabel>
+                  <DropdownControl id="percentOrRateOption" disabled={ isReadOnly } title={ this.state.ui.percentOrRateOption } updateState={ this.updateUIState }
+                    items={[ PERCENT_RATE, DOLLAR_RATE ]} />
+                </FormGroup>
+              </Col>
+              <Col md={2}>
+                <FormGroup>
+                  <ControlLabel>&nbsp;</ControlLabel>
+                  <FormControl.Static id="dollar-value" title={ formatCurrency(this.dollarValue()) }>{ formatCurrency(this.dollarValue()) }</FormControl.Static>
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row>
+              <Col md={12}>
+                <FormGroup controlId={`comment${key}`} validationState={ this.state.forms[key].commentError ? 'error' : null }>
+                  <ControlLabel>Comment</ControlLabel>
+                  <FormInputControl componentClass="textarea" value={ this.state.forms[key].comment } readOnly={ isReadOnly } updateState={ this.updateState } />
+                  <HelpBlock>{ this.state.forms[key].commentError }</HelpBlock>
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row>
+              <Col md={12}>
+                <FormGroup controlId="includeInTotal">
+                  <CheckboxControl id="includeInTotal" checked={ this.state.includeInTotal } updateState={ this.updateState }>Include in total</CheckboxControl>
+                </FormGroup>
+              </Col>
+            </Row>
+          </Grid>
+          <hr />
+        </Form>
+        );
+      })}
+      { this.state.numberOfInputs < 10 && 
+        <Button 
+          bsSize="xsmall"
+          onClick={ this.addInput }
+        >
+          <Glyphicon glyph="plus" />&nbsp;<strong>Add</strong>
+        </Button>
+      }
+      { this.state.numberOfInputs > 1 &&
+        <Button 
+          bsSize="xsmall"
+          className="remove-btn"
+          onClick={ this.removeInput }
+        >
+          <Glyphicon glyph="minus" />&nbsp;<strong>Remove</strong>
+        </Button>
+      }
     </EditDialog>;
   },
 });
