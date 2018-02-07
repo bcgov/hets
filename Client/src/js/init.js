e184ceb630379fdc6919316e18d371a20b32f7f9
Paul Litton <paul.m.litton@gmail.com>
2018-02-07 00:37:23 -0800

    Revert "HETS-457 - Create a printable Rental agreement"
    This reverts commit 66ba98420d97e7057efb5eed6e6a4bc85769234c.

--- Client/src/js/init.js
+++ Client/src/js/init.js
@@ -40,9 +40,6 @@ export default function startApp() {
   if (location.hostname === 'localhost' && process.env.DEV_USER) { //eslint-disable-line
     Api.setDevUser(process.env.DEV_USER); //eslint-disable-line
   }
-  
-  Api.setDevUser('plitton');
-  
   incrementProgressBar(5);
   // Load groups so we can check for membership
   Api.getGroups().then(() => {
