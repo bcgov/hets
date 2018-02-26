a6d7604704b40cc93e1c760be227153b5eb8b06d
Paul Litton <paul.m.litton@gmail.com>
2018-02-26 13:31:47 -0800

    HETS-495 - Review Security on Endpoints

--- Server/src/HETSAPI/Controllers/EquipmentAttachmentController.cs
+++ Server/src/HETSAPI/Controllers/EquipmentAttachmentController.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using HETSAPI.Models;
@@ -35,78 +34,6 @@ namespace HETSAPI.Controllers
         public virtual IActionResult EquipmentAttachmentsBulkPost([FromBody]EquipmentAttachment[] items)
         {
             return _service.EquipmentAttachmentsBulkPostAsync(items);
-        }
-
-        /// <summary>
-        /// Get all equipment attachments
-        /// </summary>
-        /// <response code="200">OK</response>
-        [HttpGet]
-        [Route("/api/equipmentAttachments")]
-        [SwaggerOperation("EquipmentAttachmentsGet")]
-        [SwaggerResponse(200, type: typeof(List<EquipmentAttachment>))]
-        public virtual IActionResult EquipmentAttachmentsGet()
-        {
-            return _service.EquipmentAttachmentsGetAsync();
-        }
-
-        /// <summary>
-        /// Delete equipment attachment
-        /// </summary>
-        /// <param name="id">id of EquipmentAttachment to delete</param>
-        /// <response code="200">OK</response>
-        /// <response code="404">EquipmentAttachment not found</response>
-        [HttpPost]
-        [Route("/api/equipmentAttachments/{id}/delete")]
-        [SwaggerOperation("EquipmentAttachmentsIdDeletePost")]
-        public virtual IActionResult EquipmentAttachmentsIdDeletePost([FromRoute]int id)
-        {
-            return _service.EquipmentAttachmentsIdDeletePostAsync(id);
-        }
-
-        /// <summary>
-        /// Get equipment attachment by id
-        /// </summary>
-        /// <param name="id">id of EquipmentAttachment to fetch</param>
-        /// <response code="200">OK</response>
-        /// <response code="404">EquipmentAttachment not found</response>
-        [HttpGet]
-        [Route("/api/equipmentAttachments/{id}")]
-        [SwaggerOperation("EquipmentAttachmentsIdGet")]
-        [SwaggerResponse(200, type: typeof(EquipmentAttachment))]
-        public virtual IActionResult EquipmentAttachmentsIdGet([FromRoute]int id)
-        {
-            return _service.EquipmentAttachmentsIdGetAsync(id);
-        }
-
-        /// <summary>
-        /// Update equipment attachment
-        /// </summary>
-        /// <param name="id">id of EquipmentAttachment to update</param>
-        /// <param name="item"></param>
-        /// <response code="200">OK</response>
-        /// <response code="404">EquipmentAttachment not found</response>
-        [HttpPut]
-        [Route("/api/equipmentAttachments/{id}")]
-        [SwaggerOperation("EquipmentAttachmentsIdPut")]
-        [SwaggerResponse(200, type: typeof(EquipmentAttachment))]
-        public virtual IActionResult EquipmentAttachmentsIdPut([FromRoute]int id, [FromBody]EquipmentAttachment item)
-        {
-            return _service.EquipmentAttachmentsIdPutAsync(id, item);
-        }
-
-        /// <summary>
-        /// Create equipment attachment
-        /// </summary>
-        /// <param name="item"></param>
-        /// <response code="201">EquipmentAttachment created</response>
-        [HttpPost]
-        [Route("/api/equipmentAttachments")]
-        [SwaggerOperation("EquipmentAttachmentsPost")]
-        [SwaggerResponse(200, type: typeof(EquipmentAttachment))]
-        public virtual IActionResult EquipmentAttachmentsPost([FromBody]EquipmentAttachment item)
-        {
-            return _service.EquipmentAttachmentsPostAsync(item);
-        }
+        }        
     }
 }
