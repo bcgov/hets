9fd2511a28dc3e7d76f9a06720513d85526cdf10
Paul Litton <paul.m.litton@gmail.com>
2018-02-26 11:50:10 -0800

    HETS-495 - Review Security on Endpoints (WIP) Beginning to cleanup endpoints (remove unused and add permissions to remaining)

--- Server/src/HETSAPI/Controllers/CityController.cs
+++ Server/src/HETSAPI/Controllers/CityController.cs
@@ -45,68 +45,10 @@ namespace HETSAPI.Controllers
         [Route("/api/cities")]
         [SwaggerOperation("CitiesGet")]
         [SwaggerResponse(200, type: typeof(List<City>))]
+        [RequiresPermission(Permission.Login)]
         public virtual IActionResult CitiesGet()
         {
             return _service.CitiesGetAsync();
-        }
-
-        /// <summary>
-        /// Delete city
-        /// </summary>
-        /// <param name="id">id of City to delete</param>
-        /// <response code="200">OK</response>
-        /// <response code="404">City not found</response>
-        [HttpPost]
-        [Route("/api/cities/{id}/delete")]
-        [SwaggerOperation("CitiesIdDeletePost")]
-        public virtual IActionResult CitiesIdDeletePost([FromRoute]int id)
-        {
-            return _service.CitiesIdDeletePostAsync(id);
-        }
-
-        /// <summary>
-        /// Get city by id
-        /// </summary>
-        /// <param name="id">id of City to fetch</param>
-        /// <response code="200">OK</response>
-        /// <response code="404">City not found</response>
-        [HttpGet]
-        [Route("/api/cities/{id}")]
-        [SwaggerOperation("CitiesIdGet")]
-        [SwaggerResponse(200, type: typeof(City))]
-        public virtual IActionResult CitiesIdGet([FromRoute]int id)
-        {
-            return _service.CitiesIdGetAsync(id);
-        }
-
-        /// <summary>
-        /// Update city
-        /// </summary>
-        /// <param name="id">id of City to update</param>
-        /// <param name="item"></param>
-        /// <response code="200">OK</response>
-        /// <response code="404">City not found</response>
-        [HttpPut]
-        [Route("/api/cities/{id}")]
-        [SwaggerOperation("CitiesIdPut")]
-        [SwaggerResponse(200, type: typeof(City))]
-        public virtual IActionResult CitiesIdPut([FromRoute]int id, [FromBody]City item)
-        {
-            return _service.CitiesIdPutAsync(id, item);
-        }
-
-        /// <summary>
-        /// Create city
-        /// </summary>
-        /// <param name="item"></param>
-        /// <response code="201">City created</response>
-        [HttpPost]
-        [Route("/api/cities")]
-        [SwaggerOperation("CitiesPost")]
-        [SwaggerResponse(200, type: typeof(City))]
-        public virtual IActionResult CitiesPost([FromBody]City item)
-        {
-            return _service.CitiesPostAsync(item);
-        }
+        }        
     }
 }
--- Server/src/HETSAPI/Controllers/DistrictController.cs
+++ Server/src/HETSAPI/Controllers/DistrictController.cs
@@ -45,84 +45,11 @@ namespace HETSAPI.Controllers
         [Route("/api/districts")]
         [SwaggerOperation("DistrictsGet")]
         [SwaggerResponse(200, type: typeof(List<District>))]
+        [RequiresPermission(Permission.Login)]
         public virtual IActionResult DistrictsGet()
         {
             return _service.DistrictsGetAsync();
-        }
-
-        /// <summary>
-        /// Delete district
-        /// </summary>
-        /// <param name="id">id of District to delete</param>
-        /// <response code="200">OK</response>
-        /// <response code="404">District not found</response>
-        [HttpPost]
-        [Route("/api/districts/{id}/delete")]
-        [SwaggerOperation("DistrictsIdDeletePost")]
-        public virtual IActionResult DistrictsIdDeletePost([FromRoute]int id)
-        {
-            return _service.DistrictsIdDeletePostAsync(id);
-        }
-
-        /// <summary>
-        /// Get district by id
-        /// </summary>
-        /// <param name="id">id of District to fetch</param>
-        /// <response code="200">OK</response>
-        /// <response code="404">District not found</response>
-        [HttpGet]
-        [Route("/api/districts/{id}")]
-        [SwaggerOperation("DistrictsIdGet")]
-        [SwaggerResponse(200, type: typeof(District))]
-        public virtual IActionResult DistrictsIdGet([FromRoute]int id)
-        {
-            return _service.DistrictsIdGetAsync(id);
-        }
-
-        /// <summary>
-        /// Update district
-        /// </summary>
-        /// <param name="id">id of District to update</param>
-        /// <param name="item"></param>
-        /// <response code="200">OK</response>
-        /// <response code="404">District not found</response>
-        [HttpPut]
-        [Route("/api/districts/{id}")]
-        [SwaggerOperation("DistrictsIdPut")]
-        [SwaggerResponse(200, type: typeof(District))]
-        public virtual IActionResult DistrictsIdPut([FromRoute]int id, [FromBody]District item)
-        {
-            return _service.DistrictsIdPutAsync(id, item);
-        }
-
-        /// <summary>
-        /// Get service areas associated with a district
-        /// </summary>
-        /// <remarks>Returns the Service Areas for a specific region</remarks>
-        /// <param name="id">id of District for which to fetch the ServiceAreas</param>
-        /// <response code="200">OK</response>
-        [HttpGet]
-        [Route("/api/districts/{id}/serviceareas")]
-        [SwaggerOperation("DistrictsIdServiceareasGet")]
-        [SwaggerResponse(200, type: typeof(List<ServiceArea>))]
-        public virtual IActionResult DistrictsIdServiceareasGet([FromRoute]int id)
-        {
-            return _service.DistrictsIdServiceareasGetAsync(id);
-        }
-
-        /// <summary>
-        /// Create district
-        /// </summary>
-        /// <param name="item"></param>
-        /// <response code="201">District created</response>
-        [HttpPost]
-        [Route("/api/districts")]
-        [SwaggerOperation("DistrictsPost")]
-        [SwaggerResponse(200, type: typeof(District))]
-        public virtual IActionResult DistrictsPost([FromBody]District item)
-        {
-            return _service.DistrictsPostAsync(item);
-        }
+        }        
 
         /// <summary>
         /// Get all owners by distict (minimal data returned) - lookup
@@ -132,6 +59,7 @@ namespace HETSAPI.Controllers
         [Route("/api/district/{id}/owners")]
         [SwaggerOperation("DistrictOwnersGet")]
         [SwaggerResponse(200, type: typeof(List<Owner>))]
+        [RequiresPermission(Permission.Login)]
         public virtual IActionResult DistrictOwnersGet([FromRoute]int id)
         {
             return _service.DistrictOwnersGetAsync(id);
