9d5833a0cd16d1060e3fa800cf6930879e0b9fe9
Kieran <kieran_summerfeldt@hotmail.com>
2018-02-28 11:01:33 -0800

    Replaced local areas call with local areas by district call. Updated local areas filtering in componenets.

--- Client/src/js/views/Owners.jsx
+++ Client/src/js/views/Owners.jsx
@@ -170,7 +170,6 @@ var Owners = React.createClass({
   render() {
     // Constrain the local area drop downs to those in the District of the current logged in user
     var localAreas = _.chain(this.props.localAreas)
-      .filter(localArea => localArea.serviceArea && localArea.serviceArea.district.id == this.props.currentUser.district.id)
       .sortBy('name')
       .value();
 
