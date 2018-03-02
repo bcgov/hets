7e5e6ce31d072f21fe303a38c5bbb1a220dd8037
Paul Litton <paul.m.litton@gmail.com>
2018-03-01 16:15:30 -0800

    HETS-566 - Clone rental agreement - can not edit / delete copied infromation

--- Server/src/HETSAPI/Controllers/RentalAgreementConditionController.cs
+++ Server/src/HETSAPI/Controllers/RentalAgreementConditionController.cs
@@ -35,5 +35,35 @@ namespace HETSAPI.Controllers
         {
             return _service.RentalagreementconditionsBulkPostAsync(items);
         }                
+
+        /// <summary>	
+        /// Delete rental agreement condition	
+        /// </summary>	
+        /// <param name="id">id of RentalAgreementCondition to delete</param>	
+        /// <response code="200">OK</response>	
+        [HttpPost]	
+        [Route("/api/rentalagreementconditions/{id}/delete")]	
+        [SwaggerOperation("RentalagreementconditionsIdDeletePost")]
+        [RequiresPermission(Permission.Login)]
+        public virtual IActionResult RentalagreementconditionsIdDeletePost([FromRoute]int id)
+        {	
+            return _service.RentalagreementconditionsIdDeletePostAsync(id);
+        }
+
+        /// <summary>	
+        /// Update rental agreement condition by id	
+        /// </summary>	
+        /// <param name="id">id of RentalAgreementCondition to fetch</param>	
+        /// <param name="item"></param>	
+        /// <response code="200">OK</response>	
+        [HttpPut]	
+        [Route("/api/rentalagreementconditions/{id}")]	
+        [SwaggerOperation("RentalagreementconditionsIdPut")]	
+        [SwaggerResponse(200, type: typeof(RentalAgreementCondition))]
+        [RequiresPermission(Permission.Login)]
+        public virtual IActionResult RentalagreementconditionsIdPut([FromRoute]int id, [FromBody]RentalAgreementCondition item)
+        {	
+            return _service.RentalagreementconditionsIdPutAsync(id, item);	
+        }
     }
 }
