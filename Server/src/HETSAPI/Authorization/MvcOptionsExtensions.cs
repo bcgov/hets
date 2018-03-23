d1b519343c3dfe831f8ca16f9a3695ecfdaf1539
Paul Litton <paul.m.litton@gmail.com>
2018-03-23 08:36:30 -0700

    Resolving error accessging hangfire console

--- Server/src/HETSAPI/Authorization/MvcOptionsExtensions.cs
+++ Server/src/HETSAPI/Authorization/MvcOptionsExtensions.cs
@@ -16,11 +16,11 @@ namespace HETSAPI.Authorization
         /// <param name="options"></param>
         /// <returns></returns>
         public static MvcOptions AddDefaultAuthorizationPolicyFilter(this MvcOptions options)
-        {
+        {            
             // Default authorization policy enforced via a global authorization filter
             AuthorizationPolicy requireLoginPolicy = new AuthorizationPolicyBuilder()
                 .RequireAuthenticatedUser()
-                .RequireAssertion(
+                .RequireAssertion(                    
                     context => context.User.HasOnePermission(Permission.Login, Permission.BusinessLogin, Permission.ImportData)
                     )
                 .Build();
