c7a7300117b6774a456ea9adcc7c3425c8b404ce
Stephen Curran <swcurran@gmail.com>
2018-03-27 11:55:51 -0700

    HETS-625 Fix backup script, add period, retention env vars

--- Backup/backup.sh
+++ Backup/backup.sh
@@ -2,18 +2,22 @@
 # Postgresql automated backup script
 # See README.md for documentation on this script
 
-FINAL_BACKUP_DIR=$BACKUP_DIR"`date +\%Y-\%m-\%d`/"
-DBFILE=$FINAL_BACKUP_DIR"$POSTGRESQL_DATABASE`date +\%Y-\%m-\%d-%H-%M`"
-echo "Making backup directory in $FINAL_BACKUP_DIR"
- 
-if ! mkdir -p $FINAL_BACKUP_DIR; then
-	echo "Cannot create backup directory in $FINAL_BACKUP_DIR." 1>&2
-	exit 1;
-fi;
-
-export PGPASSWORD=$POSTGRESQL_PASSWORD
+export NUM_BACKUPS="${NUM_BACKUPS:-31}"
+export BACKUP_PERIOD="${BACKUP_PERIOD:-1d}"
 
 while true; do
+	FINAL_BACKUP_DIR=$BACKUP_DIR"`date +\%Y-\%m-\%d`/"
+	DBFILE=$FINAL_BACKUP_DIR"$POSTGRESQL_DATABASE`date +\%Y-\%m-\%d-%H-%M`"
+	echo "Making backup directory in $FINAL_BACKUP_DIR"
+	
+	if ! mkdir -p $FINAL_BACKUP_DIR; then
+		echo "Cannot create backup directory in $FINAL_BACKUP_DIR." 1>&2
+		exit 1;
+	fi;
+
+	export PGPASSWORD=$POSTGRESQL_PASSWORD
+
+
 
 	if ! pg_dump -Fp -h "$DATABASE_SERVICE_NAME" -U "$POSTGRESQL_USER" "$POSTGRESQL_DATABASE" | gzip > $DBFILE.sql.gz.in_progress; then
 		echo "[!!ERROR!!] Failed to backup database $POSTGRESQL_DATABASE" 
@@ -22,10 +26,13 @@ while true; do
 		echo "Database backup written to $DBFILE.sql.gz"
 		
 		# cull backups older than 31 days.  (SB-331)
-		find $BACKUP_DIR* -type d -ctime +31 | xargs rm -rf
+		find $BACKUP_DIR* -type d -ctime +${NUM_BACKUPS} | xargs rm -rf
 	fi;
+	echo "Current Backups:"
+	ls -alh ${BACKUP_DIR}/*/*sql.gz*
+	echo "===================="
 
 	# 24 hrs
-	sleep 1d
+	sleep ${BACKUP_PERIOD}
 
 done
