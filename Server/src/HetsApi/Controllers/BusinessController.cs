682aba1421d91cbcfa59fac4626c6e2420d8acbb
Paul Litton <paul.m.litton@gmail.com>
2018-09-17 12:28:03 -0700

    Added new Business Shared Key validation service

--- Server/src/HetsApi/Controllers/BusinessController.cs
+++ Server/src/HetsApi/Controllers/BusinessController.cs
@@ -67,5 +67,56 @@ namespace HetsApi.Controllers
 
             return new ObjectResult(new HetsResponse(business));
         }
+
+        /// <summary>
+        /// Validate owner shared key - link to business
+        /// </summary>
+        /// <param name="id">id of Business to fetch</param>
+        /// <param name="sharedKey"></param>
+        [HttpGet]
+        [Route("{id}/validateOwner")]
+        [SwaggerOperation("BusinessIdValidateOwner")]
+        [SwaggerResponse(200, type: typeof(HetBusiness))]
+        [RequiresPermission(HetPermission.BusinessLogin)]
+        public virtual IActionResult BusinessIdValidateOwner([FromRoute]int id, [FromQuery]string sharedKey)
+        {
+            if (string.IsNullOrEmpty(sharedKey))
+            {
+                // shared key not provided
+                return new ObjectResult(new HetsResponse("HETS-19", ErrorViewModel.GetDescription("HETS-19", _configuration)));
+            }
+
+            bool exists = _context.HetBusiness.Any(a => a.BusinessId == id);
+
+            // not found
+            if (!exists) return new ObjectResult(new HetsResponse("HETS-01", ErrorViewModel.GetDescription("HETS-01", _configuration)));
+
+            // find owner using shred key (exact match)
+            HetOwner owner = _context.HetOwner.FirstOrDefault(a => a.SharedKey.Equals(sharedKey));
+
+            // validate the key
+            if (owner == null)
+            {
+                // shared key not found
+                return new ObjectResult(new HetsResponse("HETS-20", ErrorViewModel.GetDescription("HETS-20", _configuration)));
+            }
+
+            if (owner.BusinessId != null && owner.BusinessId != id)
+            {
+                // shared key already used
+                return new ObjectResult(new HetsResponse("HETS-21", ErrorViewModel.GetDescription("HETS-21", _configuration)));
+            }
+
+            // update owner
+            owner.BusinessId = id;
+            _context.SaveChanges();
+
+            // get updated business record and return to the UI
+            HetBusiness business = _context.HetBusiness.AsNoTracking()
+                .Include(x => x.HetOwner)
+                .FirstOrDefault(a => a.BusinessId == id);
+
+            return new ObjectResult(new HetsResponse(business));
+        }
     }
 }
