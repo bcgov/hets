fea85628b7891c7e4a3b87fa7f14abc874e34e12
Paul Litton <paul.m.litton@gmail.com>
2018-09-20 12:01:24 -0700

    HETS-678 - Error when creating/updating anything

--- Common/src/HetsData/Model/HetDistrict.cs
+++ Common/src/HetsData/Model/HetDistrict.cs
@@ -39,7 +39,6 @@ namespace HetsData.Model
         [JsonIgnore]public string DbLastUpdateUserId { get; set; }
         public int ConcurrencyControlNumber { get; set; }
 
-        [JsonIgnore]
         public HetRegion Region { get; set; }
 
         [JsonIgnore]
--- Common/src/HetsData/Model/HetLocalArea.cs
+++ Common/src/HetsData/Model/HetLocalArea.cs
@@ -37,7 +37,6 @@ namespace HetsData.Model
         [JsonIgnore]public string DbLastUpdateUserId { get; set; }
         public int ConcurrencyControlNumber { get; set; }
 
-        [JsonIgnore]
         public HetServiceArea ServiceArea { get; set; }
 
         [JsonIgnore]
