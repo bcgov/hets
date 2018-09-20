615037fc4ad164df01d4458ef7f02ee43046b910
Mike Lyttle <mikelyttle@gmail.com>
2018-09-20 12:11:39 -0700

    fix owner policy info not populating

--- Client/src/js/api.js
+++ Client/src/js/api.js
@@ -609,6 +609,15 @@ function parseOwner(owner) {
   owner.equipmentList = owner.equipment;
   delete owner.equipment;
 
+  owner.workSafeBCExpiryDate = owner.workSafeBcexpiryDate;
+  delete owner.workSafeBcexpiryDate;
+
+  owner.workSafeBCPolicyNumber = owner.workSafeBcpolicyNumber;
+  delete owner.workSafeBcpolicyNumber;
+  
+  owner.cglEndDate = owner.cglendDate;
+  delete owner.cglendDate;
+
   if (!owner.localArea) { owner.localArea = { id: 0, name: '' }; }
   if (!owner.localArea.serviceArea) { owner.localArea.serviceArea = { id: 0, name: '' }; }
   if (!owner.localArea.serviceArea.district) { owner.localArea.serviceArea.district = { id: 0, name: '' }; }
