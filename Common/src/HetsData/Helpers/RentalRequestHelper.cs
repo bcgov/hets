2ffcd82f440ecdaba1197b510eba41b69169c6a9
Paul Litton <paul.m.litton@gmail.com>
2018-09-23 17:53:03 -0700

    Fix buils error

--- Common/src/HetsData/Helpers/RentalRequestHelper.cs
+++ Common/src/HetsData/Helpers/RentalRequestHelper.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using HetsData.Model;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
