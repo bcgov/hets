3a3241518c8a6c4ea75b6ed653520e7ec9c54a4c
Paul Litton <paul.m.litton@gmail.com>
2018-10-11 13:50:32 -0700

    Completed annual rollover functionality

--- Common/src/HetsData/Helpers/AnnualRolloverHelper.cs
+++ Common/src/HetsData/Helpers/AnnualRolloverHelper.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using Hangfire.Console;
+using Hangfire.Server;
+using Hangfire.Console.Progress;
 using Microsoft.EntityFrameworkCore;
 using HetsData.Model;
 
@@ -46,8 +50,7 @@ namespace HetsData.Helpers
                     DistrictId = id,
                     CurrentFiscalYear = fiscalStart.Year,
                     NextFiscalYear = fiscalStart.Year + 1,
-                    DisplayRolloverMessage = false,
-                    ProgressPercentage = 100
+                    DisplayRolloverMessage = false
                 };
 
                 context.HetDistrictStatus.Add(status);
@@ -64,38 +67,78 @@ namespace HetsData.Helpers
 
         #endregion
 
-        /*
+        #region Annual Rollover Process        
+
         /// <summary>
-        /// Hangfire job to do the Annual Rollover tasks
+        /// Annual Rollover
         /// </summary>
         /// <param name="context"></param>
+        /// <param name="districtId"></param>        
+        /// <param name="seniorityScoringRules"></param>
         /// <param name="connectionString"></param>
-        /// <param name="configuration"></param>
-        public static void AnnualRolloverJob(PerformContext context, string connectionString, IConfiguration configuration)
+        public static void AnnualRolloverJob(PerformContext context, int districtId, string seniorityScoringRules, string connectionString)
         {
             try
-            {
+            {                
                 // open a connection to the database
-                DbContextOptionsBuilder<DbAppContext> options = new DbContextOptionsBuilder<DbAppContext>();
-                options.UseNpgsql(connectionString);
-                DbAppContext dbContext = new DbAppContext(null, options.Options);
+                DbAppContext dbContext = new DbAppContext(connectionString);
 
                 // get processing rules
-                SeniorityScoringRules scoringRules = new SeniorityScoringRules(configuration);
+                SeniorityScoringRules scoringRules = new SeniorityScoringRules(seniorityScoringRules);
 
                 // update progress bar
                 IProgressBar progress = context.WriteProgressBar();
-                context.WriteLine("Starting Annual Rollover Job");
+                context.WriteLine("Starting Annual Rollover Job - District #" + districtId);
 
                 progress.SetValue(0);
 
-                // get all equipment types
-                List<EquipmentType> equipmentTypes = dbContext.EquipmentTypes.ToList();
+                // validate district id
+                HetDistrict district = dbContext.HetDistrict.AsNoTracking()
+                    .FirstOrDefault(x => x.DistrictId == districtId);
 
-                // The annual rollover will process all local areas in turn
-                List<LocalArea> localAreas = dbContext.LocalAreas.ToList();
+                if (district == null)
+                {
+                    context.WriteLine("District not found");
+                    progress.SetValue(100);
+                    return;
+                }
 
-                foreach (LocalArea localArea in localAreas.WithProgress(progress))
+                // get status record - and ensure we're active
+                HetDistrictStatus status = GetRecord(districtId, dbContext);
+
+                if (status == null)
+                {
+                    context.WriteLine("District Status not found");
+                    progress.SetValue(100);
+                    return;
+                }
+                
+                // get equipment status
+                int? statusId = StatusHelper.GetStatusId(HetEquipment.StatusApproved, "equipmentStatus", dbContext);
+                if (statusId == null)
+                {
+                    context.WriteLine("Equipment Status not found");
+                    progress.SetValue(100);
+                    return;                    
+                }
+
+                // get all district equipment types
+                List<HetDistrictEquipmentType> equipmentTypes = dbContext.HetDistrictEquipmentType.AsNoTracking()
+                    .Include(x => x.EquipmentType)
+                    .Where(x => x.DistrictId == districtId).ToList();
+
+                // get all local areas
+                List<HetLocalArea> localAreas = dbContext.HetLocalArea.AsNoTracking()
+                    .Where(a => a.ServiceArea.DistrictId == districtId).ToList();
+
+                // update status - job is kicked off
+                int localAreaCompleteCount = 0;
+                int equipmentCompleteCount = 0;
+
+                UpdateStatusKickoff(dbContext, status, localAreaCompleteCount, equipmentCompleteCount);
+
+                // process all local areas and equipment types
+                foreach (HetLocalArea localArea in localAreas.WithProgress(progress))
                 {
                     if (localArea.Name != null)
                     {
@@ -103,36 +146,38 @@ namespace HetsData.Helpers
                     }
                     else
                     {
-                        context.WriteLine("Local Area ID: " + localArea.Id);
+                        context.WriteLine("Local Area ID: " + localArea.LocalAreaId);
                     }
 
-                    foreach (EquipmentType equipmentType in equipmentTypes)
+                    // reset equipment counter
+                    equipmentCompleteCount = 0;
+
+                    foreach (HetDistrictEquipmentType equipmentType in equipmentTypes)
                     {
                         // it this a dump truck? 
-                        bool isDumpTruck = equipmentType.IsDumpTruck;
+                        bool isDumpTruck = equipmentType.EquipmentType.IsDumpTruck;
 
                         // get rules for scoring and seniority block
                         int seniorityScoring = isDumpTruck ? scoringRules.GetEquipmentScore("DumpTruck") : scoringRules.GetEquipmentScore();
                         int blockSize = isDumpTruck ? scoringRules.GetBlockSize("DumpTruck") : scoringRules.GetBlockSize();
                         int totalBlocks = isDumpTruck ? scoringRules.GetTotalBlocks("DumpTruck") : scoringRules.GetTotalBlocks();
 
-                        using (DbAppContext etContext = new DbAppContext(null, options.Options))
+                        using (DbAppContext etContext = new DbAppContext(connectionString))
                         {
-                            List<Equipment> data = etContext.Equipments
+                            List<HetEquipment> data = etContext.HetEquipment
                                 .Include(x => x.LocalArea)
                                 .Include(x => x.DistrictEquipmentType.EquipmentType)
-                                .Where(x => x.Status == Equipment.StatusApproved &&
-                                            x.LocalArea.Id == localArea.Id &&
-                                            x.DistrictEquipmentType.EquipmentType.Id == equipmentType.Id)
-                                .Select(x => x)
+                                .Where(x => x.EquipmentStatusTypeId == statusId &&
+                                            x.LocalAreaId == localArea.LocalAreaId &&
+                                            x.DistrictEquipmentTypeId == equipmentType.DistrictEquipmentTypeId)
                                 .ToList();
 
-                            foreach (Equipment equipment in data)
+                            foreach (HetEquipment equipment in data)
                             {
                                 // rollover the year
                                 equipment.ServiceHoursThreeYearsAgo = equipment.ServiceHoursTwoYearsAgo;
                                 equipment.ServiceHoursTwoYearsAgo = equipment.ServiceHoursLastYear;
-                                equipment.ServiceHoursLastYear = equipment.GetYtdServiceHours(dbContext);
+                                equipment.ServiceHoursLastYear = EquipmentHelper.GetYtdServiceHours(equipment.EquipmentId, dbContext);
                                 equipment.CalculateYearsOfService(DateTime.UtcNow);
 
                                 // blank out the override reason
@@ -141,22 +186,34 @@ namespace HetsData.Helpers
                                 // update the seniority score
                                 equipment.CalculateSeniority(seniorityScoring);
 
-                                etContext.Equipments.Update(equipment);
+                                etContext.HetEquipment.Update(equipment);
                                 etContext.SaveChanges();
-                                etContext.Entry(equipment).State = EntityState.Detached;
                             }
                         }
 
                         // now update the rotation list
-                        using (DbAppContext abContext = new DbAppContext(null, options.Options))
+                        using (DbAppContext abContext = new DbAppContext(connectionString))
                         {
-                            int localAreaId = localArea.Id;
-                            int equipmentTypeId = equipmentType.Id;
+                            int localAreaId = localArea.LocalAreaId;
+                            int equipmentTypeId = equipmentType.DistrictEquipmentTypeId;
 
-                            AssignBlocks(abContext, localAreaId, equipmentTypeId, blockSize, totalBlocks);
+                            SeniorityListHelper.AssignBlocks(localAreaId, equipmentTypeId, blockSize, totalBlocks, abContext);
                         }
+
+                        // increment counters and update status
+                        equipmentCompleteCount++;
+                        UpdateStatus(dbContext, status, localAreaCompleteCount, equipmentCompleteCount);
                     }
+
+                    // increment counters and update status
+                    localAreaCompleteCount++;
+                    UpdateStatus(dbContext, status, localAreaCompleteCount, equipmentCompleteCount);
+                    if (status.ProgressPercentage != null) progress.SetValue((int)status.ProgressPercentage);
                 }
+
+                // done!
+                UpdateStatusComplete(dbContext, status, localAreaCompleteCount, equipmentCompleteCount);
+                progress.SetValue(100);
             }
             catch (Exception e)
             {
@@ -164,6 +221,104 @@ namespace HetsData.Helpers
                 throw;
             }
         }
-        */
+
+        private static void UpdateStatus(DbAppContext dbContext, HetDistrictStatus status,
+            int localAreaCompleteCount, int equipmentCompleteCount)
+        {
+            try
+            {
+                int localAreaCount = status.LocalAreaCount ?? 0;
+                int equipmentCount = status.DistrictEquipmentTypeCount ?? 0;
+                int percentComplete;
+
+                if (localAreaCount == 0 &&
+                    equipmentCount == 0)
+                {
+                    percentComplete = 100;
+                }
+                else
+                {
+                    // (current / maximum) * 100 -> just using the local area
+                    decimal temp = Convert.ToDecimal(localAreaCompleteCount) / Convert.ToDecimal(localAreaCount);
+                    temp = temp * 100;
+                    percentComplete = Convert.ToInt32(temp);
+
+                    if (percentComplete < 1) percentComplete = 1;
+                    if (percentComplete > 99) percentComplete = 100;
+
+                }
+
+                status.LocalAreaCompleteCount = localAreaCompleteCount;
+                status.DistrictEquipmentTypeCompleteCount = equipmentCompleteCount;
+                status.ProgressPercentage = percentComplete;
+                status.DisplayRolloverMessage = true;
+
+                dbContext.HetDistrictStatus.Update(status);
+                dbContext.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private static void UpdateStatusKickoff(DbAppContext dbContext, HetDistrictStatus status,
+            int localAreaCompleteCount, int equipmentCompleteCount)
+        {
+            try
+            {
+                status.LocalAreaCompleteCount = localAreaCompleteCount;
+                status.DistrictEquipmentTypeCompleteCount = equipmentCompleteCount;
+                status.ProgressPercentage = 1;
+                status.RolloverStartDate = DateTime.UtcNow;
+                status.DisplayRolloverMessage = true;
+
+                dbContext.HetDistrictStatus.Update(status);
+                dbContext.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private static void UpdateStatusComplete(DbAppContext dbContext, HetDistrictStatus status,
+            int localAreaCompleteCount, int equipmentCompleteCount)
+        {
+            try
+            {
+                // determine the current fiscal year
+                DateTime fiscalStart;
+
+                if (DateTime.UtcNow.Month == 1 || DateTime.UtcNow.Month == 2 || DateTime.UtcNow.Month == 3)
+                {
+                    fiscalStart = new DateTime(DateTime.UtcNow.AddYears(-1).Year, 4, 1);
+                }
+                else
+                {
+                    fiscalStart = new DateTime(DateTime.UtcNow.Year, 4, 1);
+                }
+
+                status.LocalAreaCompleteCount = localAreaCompleteCount;
+                status.DistrictEquipmentTypeCompleteCount = equipmentCompleteCount;
+                status.ProgressPercentage = 100;
+                status.RolloverEndDate = DateTime.UtcNow;
+                status.CurrentFiscalYear = fiscalStart.Year;
+                status.NextFiscalYear = fiscalStart.Year + 1;
+                status.DisplayRolloverMessage = true;
+
+                dbContext.HetDistrictStatus.Update(status);
+                dbContext.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        #endregion
     }
 }
--- Server/src/HetsApi/Controllers/DistrictController.cs
+++ Server/src/HetsApi/Controllers/DistrictController.cs
@@ -1,16 +1,19 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using Hangfire;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 using Swashbuckle.AspNetCore.Annotations;
 using HetsApi.Authorization;
 using HetsApi.Helpers;
 using HetsApi.Model;
 using HetsData.Helpers;
 using HetsData.Model;
-using Microsoft.AspNetCore.Authorization;
 
 namespace HetsApi.Controllers
 {
@@ -21,6 +24,7 @@ namespace HetsApi.Controllers
     [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
     public class DistrictController : Controller
     {
+        private readonly Object _thisLock = new Object();
         private readonly DbAppContext _context;
         private readonly IConfiguration _configuration;
 
@@ -162,6 +166,165 @@ namespace HetsApi.Controllers
 
             // get status of current district
             return new ObjectResult(new HetsResponse(AnnualRolloverHelper.GetRecord(id, _context)));
+        }
+
+        /// <summary>
+        /// Start the annual rollover process
+        /// </summary>
+        [HttpGet]
+        [Route("{id}/annualRollover")]
+        [SwaggerOperation("AnnualRolloverGet")]
+        [RequiresPermission(HetPermission.DistrictRollover)]
+        public virtual IActionResult AnnualRolloverGet([FromRoute]int id)
+        {
+            bool exists = _context.HetDistrict.Any(a => a.DistrictId == id);
+
+            // not found
+            if (!exists) return new ObjectResult(new HetsResponse("HETS-01", ErrorViewModel.GetDescription("HETS-01", _configuration)));
+
+            // determine the current fiscal year
+            DateTime fiscalStart;
+
+            if (DateTime.UtcNow.Month == 1 || DateTime.UtcNow.Month == 2 || DateTime.UtcNow.Month == 3)
+            {
+                fiscalStart = new DateTime(DateTime.UtcNow.AddYears(-1).Year, 4, 1);
+            }
+            else
+            {
+                fiscalStart = new DateTime(DateTime.UtcNow.Year, 4, 1);
+            }
+
+            // get record and ensure it isn't already processing
+            HetDistrictStatus status = AnnualRolloverHelper.GetRecord(id, _context);
+
+            if (status == null)
+            {
+                return new ObjectResult(new HetsResponse("HETS-01", ErrorViewModel.GetDescription("HETS-01", _configuration)));
+            }
+
+            if (status.CurrentFiscalYear == fiscalStart.Year)
+            {
+                // return - cannot rollover again
+                return new ObjectResult(status);
+            }
+
+            if (status.DisplayRolloverMessage == true ||
+                (status.ProgressPercentage != null && status.ProgressPercentage > 0))
+            {
+                // return already active
+                return new ObjectResult(status);
+            }
+
+            // serialize scoring rules from config into json string
+            IConfigurationSection scoringRules = _configuration.GetSection("SeniorityScoringRules");
+            string seniorityScoringRules = GetConfigJson(scoringRules);
+
+            // get connection string
+            string connectionString = GetConnectionString();
+
+            // queue the job
+            BackgroundJob.Enqueue(() => AnnualRolloverHelper.AnnualRolloverJob(null, id, seniorityScoringRules, connectionString));
+
+            // get counts for this district
+            int localAreaCount = _context.HetLocalArea
+                .Count(a => a.ServiceArea.DistrictId == id);
+
+            int equipmentCount = _context.HetDistrictEquipmentType
+                .Count(a => a.DistrictId == id);
+
+            // update status - job is kicked off
+            status.LocalAreaCount = localAreaCount;
+            status.DistrictEquipmentTypeCount = equipmentCount;
+            status.ProgressPercentage = 1;
+            status.DisplayRolloverMessage = true;
+
+            _context.HetDistrictStatus.Update(status);
+            _context.SaveChanges();
+
+            return new ObjectResult(status);
+        }
+
+        #endregion
+
+        #region Get Database Connection String
+
+        /// <summary>
+        /// Retrieve database connection string
+        /// </summary>
+        /// <returns></returns>
+        private string GetConnectionString()
+        {
+            string connectionString;
+
+            lock (_thisLock)
+            {
+                string host = _configuration["DATABASE_SERVICE_NAME"];
+                string username = _configuration["POSTGRESQL_USER"];
+                string password = _configuration["POSTGRESQL_PASSWORD"];
+                string database = _configuration["POSTGRESQL_DATABASE"];
+
+                if (string.IsNullOrEmpty(host) || string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password) ||
+                    string.IsNullOrEmpty(database))
+                {
+                    // When things get cleaned up properly, this is the only call we'll have to make.
+                    connectionString = _configuration.GetConnectionString("HETS");
+                }
+                else
+                {
+                    // Environment variables override all other settings; same behaviour as the configuration provider when things get cleaned up. 
+                    connectionString = $"Host={host};Username={username};Password={password};Database={database};";
+                }
+            }
+
+            return connectionString;
+        }
+
+        #endregion
+
+        #region Get Scoring Rules
+
+        private string GetConfigJson(IConfigurationSection scoringRules)
+        {
+            string jsonString = RecurseConfigJson(scoringRules);
+
+            if (jsonString.EndsWith("},"))
+            {
+                jsonString = jsonString.Substring(0, jsonString.Length - 1);
+            }
+
+            return jsonString;
+        }
+
+        private string RecurseConfigJson(IConfigurationSection scoringRules)
+        {
+            StringBuilder temp = new StringBuilder();
+
+            temp.Append("{");
+
+            // check for children
+            foreach (IConfigurationSection section in scoringRules.GetChildren())
+            {
+                temp.Append(@"""" + section.Key + @"""" + ":");
+
+                if (section.Value == null)
+                {
+                    temp.Append(RecurseConfigJson(section));
+                }
+                else
+                {
+                    temp.Append(@"""" + section.Value + @"""" + ",");
+                }
+            }
+
+            string jsonString = temp.ToString();
+
+            if (jsonString.EndsWith(","))
+            {
+                jsonString = jsonString.Substring(0, jsonString.Length - 1);
+            }
+
+            jsonString = jsonString + "},";
+            return jsonString;
         }
 
         #endregion
