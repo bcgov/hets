5aad8b9baeb41a235c1aca9c0e6e6414480e8119
Mike Lyttle <mikelyttle@gmail.com>
2018-11-20 17:11:19 -0800

    fix miscellaneous typos

--- Client/src/js/views/dialogs/RentalRequestsAddDialog.jsx
+++ Client/src/js/views/dialogs/RentalRequestsAddDialog.jsx
@@ -80,7 +80,7 @@ var RentalRequestsAddDialog = React.createClass({
     if (this.state.projectId !== 0) { return true; }
     if (this.state.localAreaId !== 0) { return true; }
     if (this.state.equipmentTypeId !== 0) { return true; }
-    if (this.state.count !== 0) { return true; }
+    if (this.state.count !== 1) { return true; }
     if (this.state.expectedHours !== this.props.rentalRequest.expectedHours) { return true; }
     if (this.state.expectedStartDate !== this.props.rentalRequest.expectedStartDate) { return true; }
     if (this.state.expectedEndDate !== this.props.rentalRequest.expectedEndDate) { return true; }
