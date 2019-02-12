92a15725dc8602972e7c88b64ff69be1c615f478
Paul Litton <paul.m.litton@gmail.com>
2019-02-11 17:13:54 -0800

    Cleaning up unused content

--- sonar-runner/scanDotNet.cmd
+++
@@ -1,6 +0,0 @@
-set datetimef=%time:~0,2%%time:~3,2%%time:~6,2%
-cd ..
-SonarQube.Scanner.MSBuild.exe begin /k:"org.sonarqube:bcgov-hets-%USERNAME%" /n:"HETS - DOTNET - %USERNAME%" /v:"%date%.%datetimef%"
-"D:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\amd64\MSBuild.exe" /t:Rebuild
-SonarQube.Scanner.MSBuild.exe end
-cd sonar-runner
