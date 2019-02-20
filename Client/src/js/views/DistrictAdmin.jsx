280fb0d05401d9e458e9107795278660cfe2ff45
Mike Lyttle <mikelyttle@gmail.com>
2019-02-20 14:47:43 -0800

    HETS-1105 add service area dropdown to equipment type dialog

--- Client/src/js/views/DistrictAdmin.jsx
+++ Client/src/js/views/DistrictAdmin.jsx
@@ -31,8 +31,9 @@ var DistrictAdmin = React.createClass({
     rentalConditions: React.PropTypes.object,
     districtEquipmentTypes: React.PropTypes.object,
     equipmentTypes: React.PropTypes.object,
-    router: React.PropTypes.object,
     uiEquipment: React.PropTypes.object,
+    serviceAreas: React.PropTypes.object,
+    router: React.PropTypes.object,
     dispatch: React.PropTypes.func,
   },
 
@@ -151,6 +152,8 @@ var DistrictAdmin = React.createClass({
     var equipmentTypes = _.chain(this.props.equipmentTypes)
       .sortBy('blueBookSection')
       .value();
+
+    var districtServiceAreas = _.filter(this.props.serviceAreas, area => area.districtId === this.props.currentUser.district.id);
 
     if (!this.props.currentUser.hasPermission(Constant.PERMISSION_DISTRICT_CODE_TABLE_MANAGEMENT) && !this.props.currentUser.hasPermission(Constant.PERMISSION_ADMIN)) {
       return (
@@ -276,6 +279,7 @@ var DistrictAdmin = React.createClass({
           onSave={this.onDistrictEquipmentTypeSave}
           districtEquipmentType={this.state.districtEquipmentType}
           equipmentTypes={equipmentTypes}
+          serviceAreas={districtServiceAreas}
         />
       }
       { this.state.showDistrictEquipmentTypeErrorDialog &&
@@ -303,6 +307,7 @@ function mapStateToProps(state) {
     rentalConditions: state.lookups.rentalConditions,
     districtEquipmentTypes: state.lookups.districtEquipmentTypes,
     equipmentTypes: state.lookups.equipmentTypes,
+    serviceAreas: state.lookups.serviceAreas,
     uiEquipment: state.ui.districtEquipment,
   };
 }
--- Client/src/js/views/dialogs/DistrictEquipmentTypeAddEditDialog.jsx
+++ Client/src/js/views/dialogs/DistrictEquipmentTypeAddEditDialog.jsx
@@ -18,6 +18,7 @@ var DistrictEquipmentTypeAddEditDialog = React.createClass({
     show: React.PropTypes.bool,
     districtEquipmentType: React.PropTypes.object,
     equipmentTypes: React.PropTypes.array,
+    serviceAreas: React.PropTypes.array,
   },
 
   getInitialState() {
@@ -27,9 +28,11 @@ var DistrictEquipmentTypeAddEditDialog = React.createClass({
       id: this.props.districtEquipmentType.id || 0,
       equipmentTypeId: this.props.districtEquipmentType.equipmentType ? this.props.districtEquipmentType.equipmentType.id : undefined,
       districtEquipmentName: this.props.districtEquipmentType.districtEquipmentName || '',
+      serviceAreaId: this.props.districtEquipmentType.serviceAreaId || undefined,
       concurrencyControlNumber: this.props.districtEquipmentType.concurrencyControlNumber || 0,
       equipmentTypeIdError: '',
       districtEquipmentNameError: '',
+      serviceAreaIdError: '',
     };
   },
 
@@ -40,8 +43,10 @@ var DistrictEquipmentTypeAddEditDialog = React.createClass({
   didChange() {
     if (this.state.isNew && this.state.equipmentTypeId !== undefined) { return true; }
     if (this.state.isNew && this.state.districtEquipmentName !== '') { return true; }
+    if (this.state.isNew && this.state.serviceAreaId !== undefined) { return true; }
     if (!this.state.isNew && this.state.equipmentTypeId !== this.props.districtEquipmentType.equipmentType.id) { return true; }
     if (!this.state.isNew && this.state.districtEquipmentName !== this.props.districtEquipmentType.districtEquipmentName) { return true; }
+    if (!this.state.isNew && this.state.serviceAreaId !== this.props.districtEquipmentType.serviceAreaId) { return true; }
 
     return false;
   },
@@ -50,6 +55,7 @@ var DistrictEquipmentTypeAddEditDialog = React.createClass({
     this.setState({
       equipmentTypeIdError: '',
       districtEquipmentNameError: '',
+      serviceAreaIdError: '',
     });
 
     var valid = true;
@@ -70,6 +76,11 @@ var DistrictEquipmentTypeAddEditDialog = React.createClass({
       valid = false;
     }
 
+    if (this.state.serviceAreaId === undefined) {
+      this.setState({ serviceAreaIdError: 'Service area is required' });
+      valid = false;
+    }
+
     return valid;
   },
 
@@ -78,6 +89,7 @@ var DistrictEquipmentTypeAddEditDialog = React.createClass({
       id: this.state.id,
       equipmentType: { id: this.state.equipmentTypeId },
       districtEquipmentName: this.state.districtEquipmentName,
+      serviceAreaId: this.state.serviceAreaId,
       concurrencyControlNumber: this.state.concurrencyControlNumber,
     });
   },
@@ -99,6 +111,14 @@ var DistrictEquipmentTypeAddEditDialog = React.createClass({
           <ControlLabel>Equipment Type/Description <sup>*</sup></ControlLabel>
           <FormInputControl type="text" value={ this.state.districtEquipmentName } updateState={ this.updateState }/>
           <HelpBlock>{ this.state.districtEquipmentNameError }</HelpBlock>
+        </FormGroup>
+        <FormGroup controlId="serviceAreaId" validationState={ this.state.serviceAreaIdError ? 'error' : null }>
+          <ControlLabel>Service Area <sup>*</sup></ControlLabel>
+          <FilterDropdown id="serviceAreaId" fieldName="id" selectedId={ this.state.serviceAreaId } updateState={ this.updateState }
+            items={ this.props.serviceAreas }
+            className="full-width"
+          />
+          <HelpBlock>{ this.state.serviceAreaIdError }</HelpBlock>
         </FormGroup>
       </Form>
     </EditDialog>;
