21e8bc64ac94caeb3dfccd888012d949860f1638
Paul Litton <paul.m.litton@gmail.com>
2019-03-08 08:08:12 -0800

    Fix url error (downloading batch reports)

--- Client/src/js/api.js
+++ Client/src/js/api.js
@@ -2321,5 +2321,5 @@ export function deleteBatchReport(reportId) {
 }
 
 export function getStatusLettersPdf(reportId) {
-  return new ApiRequest(`/api/reports/${reportId}/download`).post(null, { responseType: Constant.RESPONSE_TYPE_BLOB });
+  return new ApiRequest(`/reports/${reportId}/download`).post(null, { responseType: Constant.RESPONSE_TYPE_BLOB });
 }
