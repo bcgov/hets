b9e42d8fefb419237e8dce65163bfffc04dd7155
Séamus O'Connor <seamus.oconnor@pneumaticweb.com>
2019-03-13 12:11:19 -0700

    HETS-883 - suppresses Chrome autofill on text boxes

--- Client/src/js/components/FormInputControl.jsx
+++ Client/src/js/components/FormInputControl.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-
 import { FormControl } from 'react-bootstrap';
-
 import _ from 'lodash';
+
 
 var FormInputControl = React.createClass({
   propTypes: {
     type: React.PropTypes.string,
     updateState: React.PropTypes.func,
     autoFocus: React.PropTypes.bool,
+    autoComplete: React.PropTypes.string,
     onChange: React.PropTypes.func,
     children: React.PropTypes.node,
   },
@@ -20,37 +20,44 @@ var FormInputControl = React.createClass({
   },
 
   changed(e) {
-    // On change listener
-    if (this.props.onChange) {
-      this.props.onChange(e);
-    }
+    const { type, updateState, onChange } = this.props;
 
-    // Update state
-    if (this.props.updateState && e.target.id) {
+    // On change listener
+    if (onChange) { onChange(e); }
+
+    if (updateState && e.target.id) {
       // Use e.target.id insted of this.props.id because it comes from the controlId.
       var value = e.target.value;
-      if (this.props.type === 'number' ) {
+      if (type === 'number' ) {
         value = parseInt(value, 10);
         if (_.isNaN(value)) {
           value = '';
         }
       }
-      if (this.props.type === 'float' ) {
+      if (type === 'float' ) {
         value = parseFloat(value);
         if (_.isNaN(value)) {
           value = '';
         }
       }
-      this.props.updateState({ [e.target.id]: value });
+
+      // Update state
+      updateState({ [e.target.id]: value });
     }
   },
 
   render() {
-    var props = _.omit(this.props, 'updateState');
+    const { type, autoComplete, children } = this.props;
 
-    return <FormControl { ...props } type={ props.type === 'float' ? 'number' : props.type } onChange={ this.changed } inputRef={ ref => { this.input = ref; }}>
-      { this.props.children }
-    </FormControl>;
+    return (
+      <FormControl
+        type={type === 'float' ? 'number' : type}
+        onChange={this.changed}
+        inputRef={ref => {this.input = ref;}}
+        autoComplete={autoComplete || 'off'}>
+        {children}
+      </FormControl>
+    );
   },
 });
 
