0d847dc3742bcb220068ec17d1c073c8e0e7b664
Mike Lyttle <mikelyttle@gmail.com>
2019-04-02 18:09:12 -0700

    HETS-1193 fix duplicate serial number bypass not appearing when editing equipment

--- Client/src/js/views/dialogs/EquipmentEditDialog.jsx
+++ Client/src/js/views/dialogs/EquipmentEditDialog.jsx
@@ -49,7 +49,7 @@ class EquipmentEditDialog extends React.Component {
       pupLegalCapacity: props.equipment.pupLegalCapacity || '',
 
       serialNumberError: null,
-      duplicateSerialNumber: false,
+      duplicateSerialNumberWarning: false,
       makeError: '',
       modelError: '',
       yearError: null,
@@ -62,7 +62,7 @@ class EquipmentEditDialog extends React.Component {
 
   componentDidUpdate(prevProps, prevState) {
     if (!_.isEqual(this.state.serialNumber, prevState.serialNumber)) {
-      this.setState({ duplicateSerialNumber: false, serialNumberError: '' });
+      this.setState({ duplicateSerialNumberWarning: false, serialNumberError: '' });
     }
   }
 
@@ -129,9 +129,9 @@ class EquipmentEditDialog extends React.Component {
   };
 
   checkForDuplicatesAndSave = () => {
-    if (this.state.duplicateSerialNumber) {
+    if (this.state.duplicateSerialNumberWarning) {
       // proceed regardless of duplicates
-      this.setState({ duplicateSerialNumber: false });
+      this.setState({ duplicateSerialNumberWarning: false });
       return this.onSave();
     }
 
@@ -151,7 +151,7 @@ class EquipmentEditDialog extends React.Component {
         });
         return null;
       } else {
-        this.setState({ duplicateSerialNumber: false });
+        this.setState({ duplicateSerialNumberWarning: false });
         return this.onSave();
       }
     });
@@ -186,7 +186,7 @@ class EquipmentEditDialog extends React.Component {
 
     return <EditDialog id="equipment-edit" show={ this.props.show }
       onClose={ this.props.onClose } onSave={ this.checkForDuplicatesAndSave } didChange={ this.didChange } isValid={ this.isValid }
-      saveText={ this.state.duplicateSerialNumber ? 'Proceed Anyways' : 'Save' }
+      saveText={ this.state.duplicateSerialNumberWarning ? 'Proceed Anyways' : 'Save' }
       title={<strong>Equipment Id: <small>{ equipment.equipmentCode }</small></strong>}>
       {(() => {
         return <Form>
