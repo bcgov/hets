0ab3583807c53f62511fbe60ef860704a086ebc0
Li <Neal.Li@gov.bc.ca>
2019-05-17 15:34:04 -0700

    Updated backup container to BCDevOps backup-container

--- Backup/connections.sql
+++
@@ -1,8 +0,0 @@
-do $$
-begin 
-    IF EXISTS (SELECT datname FROM pg_database WHERE datname = 'hets') THEN
-	  -- kill sessions
-	  PERFORM pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'hets';
-	END IF;
-end
-$$
