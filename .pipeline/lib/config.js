29ba1012c7d8097440f98999646eb11abcfbba7d
Young-Jin Chung <young-jin.chung@bc.gov.ca>
2021-03-09 09:07:30 -0800

    BCDK pipeline - WIP

---
+++ .pipeline/lib/config.js
@@ -0,0 +1,56 @@
+"use strict";
+const options = require("@bcgov/pipeline-cli").Util.parseArguments();
+const changeId = options.pr; //aka pull-request
+const version = "1.0.0";
+const name = "hello";
+
+const phases = {
+  build: {
+    namespace: "tran-hets-tools",
+    name: `${name}`,
+    phase: "build",
+    changeId: changeId,
+    suffix: `-build-${changeId}`,
+    instance: `${name}-build-${changeId}`,
+    version: `${version}-${changeId}`,
+    tag: `build-${version}-${changeId}`,
+  },
+  dev: {
+    namespace: "tran-hets-dev",
+    name: `${name}`,
+    phase: "dev",
+    changeId: changeId,
+    suffix: `-dev-${changeId}`,
+    instance: `${name}-dev-${changeId}`,
+    version: `${version}-${changeId}`,
+    tag: `dev-${version}-${changeId}`,
+  },
+  test: {
+    namespace: "tran-hets-test",
+    name: `${name}`,
+    phase: "test",
+    changeId: changeId,
+    suffix: `-test`,
+    instance: `${name}-test`,
+    version: `${version}`,
+    tag: `test-${version}`,
+  },
+  prod: {
+    namespace: "tran-hets-prod",
+    name: `${name}`,
+    phase: "prod",
+    changeId: changeId,
+    suffix: `-prod`,
+    instance: `${name}-prod`,
+    version: `${version}`,
+    tag: `prod-${version}`,
+  },
+};
+
+// This callback forces the node process to exit as failure.
+process.on("unhandledRejection", (reason) => {
+  console.log(reason);
+  process.exit(1);
+});
+
+module.exports = exports = { phases, options };
