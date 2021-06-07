a30e9bac196334576fc208777436742cd4d46d1a
Young-Jin Chung <young-jin.chung@bc.gov.ca>
2021-06-07 10:10:59 -0700

    Added rollover progress table

---
+++ Server/HetsData/Model/HetRolloverProgress.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HetsData.Model
+{
+    public partial class HetRolloverProgress
+    {
+        public int? DistrictId { get; set; }
+        public int? ProgressPercentage { get; set; }
+
+        public virtual HetDistrict District { get; set; }
+    }
+}
