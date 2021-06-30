b33e4f49d04e481bc3a6d505872f8e028fb3db92
DSoLetsDev <DSO@NC055498>
2021-06-30 09:11:26 -0700

    fixed date control crash

--- client/src/js/components/DateControl.jsx
+++ client/src/js/components/DateControl.jsx
@@ -1,9 +1,9 @@
-import PropTypes from "prop-types";
-import React from "react";
-import { ControlLabel, InputGroup, Button, Glyphicon } from "react-bootstrap";
-import _ from "lodash";
-import Moment from "moment";
-import DateTime from "react-datetime";
+import PropTypes from 'prop-types';
+import React from 'react';
+import { ControlLabel, InputGroup, Button, Glyphicon } from 'react-bootstrap';
+import _ from 'lodash';
+import Moment from 'moment';
+import DateTime from 'react-datetime';
 
 class DateControl extends React.Component {
   static propTypes = {
@@ -32,14 +32,14 @@ class DateControl extends React.Component {
       return;
     }
 
-    var dateString = date.format(this.props.format || "YYYY-MM-DD");
+    var dateString = date.format(this.props.format || 'YYYY-MM-DD');
     this.notifyValueChanged(dateString);
   };
 
   dateBlurred = (date) => {
     // when focus leaves input, if date is invalid, reset value to empty string
     if (_.isString(date) || !date || !date.isValid()) {
-      this.notifyValueChanged("");
+      this.notifyValueChanged('');
     }
   };
 
@@ -58,17 +58,14 @@ class DateControl extends React.Component {
   };
 
   render() {
-    var date = this.props.date === "" ? null : Moment.utc(this.props.date);
-    var format = this.props.format || "YYYY-MM-DD";
+    var date = this.props.date === '' ? null : Moment.utc(this.props.date);
+    var format = this.props.format || 'YYYY-MM-DD';
 
-    var placeholder = this.props.placeholder || "yyyy-mm-dd";
+    var placeholder = this.props.placeholder || 'yyyy-mm-dd';
     var disabled = this.props.disabled;
 
     return (
-      <div
-        className={`date-control ${this.props.className || ""}`}
-        id={this.props.id}
-      >
+      <div className={`date-control ${this.props.className || ''}`} id={this.props.id}>
         {(() => {
           // Inline label
           if (this.props.label) {
@@ -92,11 +89,10 @@ class DateControl extends React.Component {
             }}
             isValidDate={this.props.isValidDate}
           />
-          <InputGroup.Button>
-            <Button disabled={disabled} onClick={this.clicked}>
-              <Glyphicon glyph="calendar" title={this.props.title} />
-            </Button>
-          </InputGroup.Button>
+
+          <Button disabled={disabled} onClick={this.clicked}>
+            <FontAwesomeIcon icon="calendar-alt" title={this.props.title} />
+          </Button>
         </InputGroup>
       </div>
     );
