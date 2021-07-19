b8cfb59fe56fd7f7b3deef3ff3bc3aa555010a2b
Young-Jin Chung <young-jin.chung@bc.gov.ca>
2021-07-19 10:01:14 -0700

    Scaffolding

--- Server/HetsData/Model/DbAppContext.cs
+++ Server/HetsData/Model/DbAppContext.cs
@@ -74,15 +74,6 @@ namespace HetsData.Model
         public virtual DbSet<HetUserFavourite> HetUserFavourites { get; set; }
         public virtual DbSet<HetUserRole> HetUserRoles { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseNpgsql("Host=localhost;Username=postgres;Password=postgres;Database=hets;Port=9010");
-            }
-        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasAnnotation("Relational:Collation", "en_US.utf8");
