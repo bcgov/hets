492f30a11bbed79b5a0b11c684d51a2051d4bb5d
DSoLetsDev <DSO@NC055498>
2021-07-23 09:13:59 -0700

    filter drop down focus fix. Also fixed multiDropdown focus issue when selecting multiple items

--- client/src/js/components/MultiDropdown.jsx
+++ client/src/js/components/MultiDropdown.jsx
@@ -25,6 +25,7 @@ class MultiDropdown extends React.Component {
     var selectedIds = props.selectedIds || [];
     var items = props.items || [];
     var fieldName = props.fieldName || 'name';
+    this.myRef = React.createRef();
 
     this.state = {
       selectedIds: selectedIds,
@@ -124,7 +125,12 @@ class MultiDropdown extends React.Component {
   };
 
   toggle = (open) => {
-    this.setState({ open: open });
+    this.setState({ open: open }, () => {
+      if (this.myRef.current) {
+        //without a delay focus will not shift to input box when using keyboard shortcuts. I think the focus is called before input is rendered.
+        setTimeout(() => this.myRef.current.focus(), 100);
+      }
+    });
   };
 
   filter = (e) => {
@@ -167,12 +173,7 @@ class MultiDropdown extends React.Component {
               type="text"
               placeholder="Search"
               onChange={this.filter}
-              ref={(ref) => {
-                if (ref) {
-                  //without a delay focus will not shift to input box when using keyboard shortcuts. I think the focus is called before input is rendered.
-                  setTimeout(() => ref.focus(), 100);
-                }
-              }}
+              ref={this.myRef}
               autoComplete="off"
               onKeyDown={this.keyDown}
             />
