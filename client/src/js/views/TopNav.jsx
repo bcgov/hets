d47a2e2ce34dbb04c9aec7599b535c59d665d0fd
DSoLetsDev <DSO@NC055498>
2021-08-10 10:57:58 -0700

    created collapsible navbar.

--- client/src/js/views/TopNav.jsx
+++ client/src/js/views/TopNav.jsx
@@ -93,110 +93,123 @@ class TopNav extends React.Component {
             </div>
           </Container>
         </Navbar>
-        <Navbar id="top-nav" className={environmentClass}>
+        <Navbar id="top-nav" expand="md" className={environmentClass}>
           <Container>
             {this.props.showNav && (
-              <Nav as="ul">
-                <Nav.Item as="li">
-                  <Nav.Link as={NavLink} to={Constant.HOME_PATHNAME} disabled={navigationDisabled}>
-                    Home
-                  </Nav.Link>
-                </Nav.Item>
-                <Nav.Item as="li">
-                  <Nav.Link as={NavLink} tag={NavItem} to={Constant.OWNERS_PATHNAME} disabled={navigationDisabled}>
-                    Owners
-                  </Nav.Link>
-                </Nav.Item>
-                <Nav.Item as="li">
-                  <Nav.Link as={NavLink} to={Constant.EQUIPMENT_PATHNAME} disabled={navigationDisabled}>
-                    Equipment
-                  </Nav.Link>
-                </Nav.Item>
-                <Nav.Item as="li">
-                  <Nav.Link as={NavLink} to={Constant.PROJECTS_PATHNAME} disabled={navigationDisabled}>
-                    Projects
-                  </Nav.Link>
-                </Nav.Item>
-                <Nav.Item as="li">
-                  <Nav.Link as={NavLink} to={Constant.RENTAL_REQUESTS_PATHNAME} disabled={navigationDisabled}>
-                    Requests
-                  </Nav.Link>
-                </Nav.Item>
-                <Nav.Item as="li">
-                  <Nav.Link as={NavLink} to={Constant.TIME_ENTRY_PATHNAME} disabled={navigationDisabled}>
-                    Time Entry
-                  </Nav.Link>
-                </Nav.Item>
-                <NavDropdown id="reports-dropdown" title="Reports" disabled={navigationDisabled} as="li">
-                  <NavDropdown.Item as={NavLink} to={Constant.AIT_REPORT_PATHNAME} disabled={navigationDisabled}>
-                    Rental Agreement Summary
-                  </NavDropdown.Item>
-                  <NavDropdown.Item as={NavLink} to={Constant.SENIORITY_LIST_PATHNAME} disabled={navigationDisabled}>
-                    Seniority List
-                  </NavDropdown.Item>
-                  <NavDropdown.Item
-                    as={NavLink}
-                    to={Constant.STATUS_LETTERS_REPORT_PATHNAME}
-                    disabled={navigationDisabled}
-                  >
-                    Status Letters / Mailing Labels
-                  </NavDropdown.Item>
-                  <NavDropdown.Item as={NavLink} to={Constant.HIRING_REPORT_PATHNAME} disabled={navigationDisabled}>
-                    Hiring Report - Not Hired / Force Hire
-                  </NavDropdown.Item>
-                  <NavDropdown.Item as={NavLink} to={Constant.OWNERS_COVERAGE_PATHNAME} disabled={navigationDisabled}>
-                    WCB / CGL Coverage
-                  </NavDropdown.Item>
-                </NavDropdown>
-                <Authorize requires={Constant.PERMISSION_DISTRICT_CODE_TABLE_MANAGEMENT}>
-                  <Nav.Item as="li">
-                    <Nav.Link as={NavLink} to={Constant.DISTRICT_ADMIN_PATHNAME} disabled={navigationDisabled}>
-                      District Admin
-                    </Nav.Link>
-                  </Nav.Item>
-                </Authorize>
-                <Authorize
-                  condition={any(
-                    Constant.PERMISSION_ADMIN,
-                    Constant.PERMISSION_USER_MANAGEMENT,
-                    Constant.PERMISSION_ROLES_AND_PERMISSIONS,
-                    Constant.PERMISSION_DISTRICT_ROLLOVER,
-                    Constant.PERMISSION_VERSION
-                  )}
-                >
-                  <NavDropdown id="admin-dropdown" title="Administration" disabled={navigationDisabled} as="li">
-                    <Authorize requires={Constant.PERMISSION_ADMIN}>
+              <>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" className="btn-custom" />
+                <Navbar.Collapse>
+                  <Nav as="ul">
+                    <Nav.Item as="li">
+                      <Nav.Link as={NavLink} to={Constant.HOME_PATHNAME} disabled={navigationDisabled}>
+                        Home
+                      </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item as="li">
+                      <Nav.Link as={NavLink} tag={NavItem} to={Constant.OWNERS_PATHNAME} disabled={navigationDisabled}>
+                        Owners
+                      </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item as="li">
+                      <Nav.Link as={NavLink} to={Constant.EQUIPMENT_PATHNAME} disabled={navigationDisabled}>
+                        Equipment
+                      </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item as="li">
+                      <Nav.Link as={NavLink} to={Constant.PROJECTS_PATHNAME} disabled={navigationDisabled}>
+                        Projects
+                      </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item as="li">
+                      <Nav.Link as={NavLink} to={Constant.RENTAL_REQUESTS_PATHNAME} disabled={navigationDisabled}>
+                        Requests
+                      </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item as="li">
+                      <Nav.Link as={NavLink} to={Constant.TIME_ENTRY_PATHNAME} disabled={navigationDisabled}>
+                        Time Entry
+                      </Nav.Link>
+                    </Nav.Item>
+                    <NavDropdown id="reports-dropdown" title="Reports" disabled={navigationDisabled} as="li">
+                      <NavDropdown.Item as={NavLink} to={Constant.AIT_REPORT_PATHNAME} disabled={navigationDisabled}>
+                        Rental Agreement Summary
+                      </NavDropdown.Item>
                       <NavDropdown.Item
                         as={NavLink}
-                        to={Constant.OVERTIME_RATES_PATHNAME}
+                        to={Constant.SENIORITY_LIST_PATHNAME}
                         disabled={navigationDisabled}
                       >
-                        Manage OT Rates
+                        Seniority List
                       </NavDropdown.Item>
-                    </Authorize>
-                    <Authorize requires={Constant.PERMISSION_USER_MANAGEMENT}>
-                      <NavDropdown.Item as={NavLink} to={Constant.USERS_PATHNAME} disabled={navigationDisabled}>
-                        User Management
+                      <NavDropdown.Item
+                        as={NavLink}
+                        to={Constant.STATUS_LETTERS_REPORT_PATHNAME}
+                        disabled={navigationDisabled}
+                      >
+                        Status Letters / Mailing Labels
                       </NavDropdown.Item>
-                    </Authorize>
-                    <Authorize requires={Constant.PERMISSION_ROLES_AND_PERMISSIONS}>
-                      <NavDropdown.Item as={NavLink} to={Constant.ROLES_PATHNAME} disabled={navigationDisabled}>
-                        Roles and Permissions
+                      <NavDropdown.Item as={NavLink} to={Constant.HIRING_REPORT_PATHNAME} disabled={navigationDisabled}>
+                        Hiring Report - Not Hired / Force Hire
                       </NavDropdown.Item>
-                    </Authorize>
-                    <Authorize requires={Constant.PERMISSION_DISTRICT_ROLLOVER}>
-                      <NavDropdown.Item as={NavLink} to={Constant.ROLLOVER_PATHNAME} disabled={navigationDisabled}>
-                        Roll Over
+                      <NavDropdown.Item
+                        as={NavLink}
+                        to={Constant.OWNERS_COVERAGE_PATHNAME}
+                        disabled={navigationDisabled}
+                      >
+                        WCB / CGL Coverage
                       </NavDropdown.Item>
+                    </NavDropdown>
+                    <Authorize requires={Constant.PERMISSION_DISTRICT_CODE_TABLE_MANAGEMENT}>
+                      <Nav.Item as="li">
+                        <Nav.Link as={NavLink} to={Constant.DISTRICT_ADMIN_PATHNAME} disabled={navigationDisabled}>
+                          District Admin
+                        </Nav.Link>
+                      </Nav.Item>
                     </Authorize>
-                    <Authorize requires={Constant.PERMISSION_VERSION}>
-                      <NavDropdown.Item as={NavLink} to={Constant.VERSION_PATHNAME} disabled={navigationDisabled}>
-                        Version Info
-                      </NavDropdown.Item>
+                    <Authorize
+                      condition={any(
+                        Constant.PERMISSION_ADMIN,
+                        Constant.PERMISSION_USER_MANAGEMENT,
+                        Constant.PERMISSION_ROLES_AND_PERMISSIONS,
+                        Constant.PERMISSION_DISTRICT_ROLLOVER,
+                        Constant.PERMISSION_VERSION
+                      )}
+                    >
+                      <NavDropdown id="admin-dropdown" title="Administration" disabled={navigationDisabled} as="li">
+                        <Authorize requires={Constant.PERMISSION_ADMIN}>
+                          <NavDropdown.Item
+                            as={NavLink}
+                            to={Constant.OVERTIME_RATES_PATHNAME}
+                            disabled={navigationDisabled}
+                          >
+                            Manage OT Rates
+                          </NavDropdown.Item>
+                        </Authorize>
+                        <Authorize requires={Constant.PERMISSION_USER_MANAGEMENT}>
+                          <NavDropdown.Item as={NavLink} to={Constant.USERS_PATHNAME} disabled={navigationDisabled}>
+                            User Management
+                          </NavDropdown.Item>
+                        </Authorize>
+                        <Authorize requires={Constant.PERMISSION_ROLES_AND_PERMISSIONS}>
+                          <NavDropdown.Item as={NavLink} to={Constant.ROLES_PATHNAME} disabled={navigationDisabled}>
+                            Roles and Permissions
+                          </NavDropdown.Item>
+                        </Authorize>
+                        <Authorize requires={Constant.PERMISSION_DISTRICT_ROLLOVER}>
+                          <NavDropdown.Item as={NavLink} to={Constant.ROLLOVER_PATHNAME} disabled={navigationDisabled}>
+                            Roll Over
+                          </NavDropdown.Item>
+                        </Authorize>
+                        <Authorize requires={Constant.PERMISSION_VERSION}>
+                          <NavDropdown.Item as={NavLink} to={Constant.VERSION_PATHNAME} disabled={navigationDisabled}>
+                            Version Info
+                          </NavDropdown.Item>
+                        </Authorize>
+                      </NavDropdown>
                     </Authorize>
-                  </NavDropdown>
-                </Authorize>
-              </Nav>
+                  </Nav>
+                </Navbar.Collapse>
+              </>
             )}
             {this.props.showNav && (
               <div id="navbar-right" className="float-right d-flex">
