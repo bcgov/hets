69dcf5c7af4e5a5b4aa1acb2ee1706ef806b962a
Young-Jin Chung <young-jin.chung@bc.gov.ca>
2021-08-11 14:44:05 -0700

    HETS-1322 rewritting HETS-1304

---
+++ Server/HetsData/Mappings/EntityToEntityProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using HetsData.Entities;
+
+namespace HetsData.Mappings
+{
+    public class EntityToEntityProfile : Profile
+    {
+        public EntityToEntityProfile()
+        {
+            SourceMemberNamingConvention = new PascalCaseNamingConvention();
+            DestinationMemberNamingConvention = new PascalCaseNamingConvention();
+
+            CreateMap<HetRentalRequestSeniorityList, HetEquipment>();
+        }
+    }
+}
