a125d5c5223823d5ec276d656cbf10dd012f6a70
DSoLetsDev <DSO@NC055498>
2021-09-21 15:23:57 -0700

    HETS-1331 WIP for confirm overlay click bug. Converting DeleteButton to a functional component

--- client/src/js/components/DeleteButton.jsx
+++ client/src/js/components/DeleteButton.jsx
@@ -1,3 +1,48 @@
+// import PropTypes from 'prop-types';
+// import React from 'react';
+// import { Button, OverlayTrigger } from 'react-bootstrap';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import _ from 'lodash';
+
+// import Confirm from '../components/Confirm.jsx';
+
+// class DeleteButton extends React.Component {
+//   static propTypes = {
+//     onConfirm: PropTypes.func.isRequired,
+//     name: PropTypes.string,
+//     hide: PropTypes.bool,
+//   };
+
+//   render() {
+//     var props = _.omit(this.props, 'onConfirm', 'hide', 'name');
+
+//     return (
+//       <OverlayTrigger
+//         placement="top"
+//         trigger={'click'}
+//         rootClose
+//         overlay={<Confirm onConfirm={this.props.onConfirm} />}
+//         transition={false} //removes findDOMNode warning for transitions
+//         delay={100} //required so the onConfirm function is fired on the Confirm overlay before closing. If transition={true} this isn't needed.
+//       >
+//         {(overlayProps) => (
+//           <Button
+//             title={`Delete ${this.props.name}`}
+//             size="sm"
+//             className={this.props.hide ? 'd-none' : 'btn-custom'}
+//             {...props}
+//             {...overlayProps}
+//           >
+//             <FontAwesomeIcon icon="trash-alt" />
+//           </Button>
+//         )}
+//       </OverlayTrigger>
+//     );
+//   }
+// }
+
+// export default DeleteButton;
+
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Button, OverlayTrigger } from 'react-bootstrap';
@@ -6,38 +51,35 @@ import _ from 'lodash';
 
 import Confirm from '../components/Confirm.jsx';
 
-class DeleteButton extends React.Component {
-  static propTypes = {
-    onConfirm: PropTypes.func.isRequired,
-    name: PropTypes.string,
-    hide: PropTypes.bool,
-  };
+DeleteButton.propTypes = {
+  onConfirm: PropTypes.func.isRequired,
+  name: PropTypes.string,
+  hide: PropTypes.bool,
+};
 
-  render() {
-    var props = _.omit(this.props, 'onConfirm', 'hide', 'name');
-
-    return (
-      <OverlayTrigger
-        placement="top"
-        trigger={'focus'}
-        overlay={<Confirm onConfirm={this.props.onConfirm} />}
-        transition={false} //removes findDOMNode warning for transitions
-        delay={200} //required so the onConfirm function is fired on the Confirm overlay before closing. If transition={true} this isn't needed.
-      >
-        {(overlayProps) => (
-          <Button
-            title={`Delete ${this.props.name}`}
-            size="sm"
-            className={this.props.hide ? 'd-none' : 'btn-custom'}
-            {...props}
-            {...overlayProps}
-          >
-            <FontAwesomeIcon icon="trash-alt" />
-          </Button>
-        )}
-      </OverlayTrigger>
-    );
-  }
+function DeleteButton({ onConfirm, hide, name, ...rest }) {
+  return (
+    <OverlayTrigger
+      placement="top"
+      trigger={'click'}
+      rootClose
+      overlay={<Confirm onConfirm={onConfirm} />}
+      transition={false} //removes findDOMNode warning for transitions
+      delay={100} //required so the onConfirm function is fired on the Confirm overlay before closing. If transition={true} this isn't needed.
+    >
+      {(overlayProps) => (
+        <Button
+          title={`Delete ${name}`}
+          size="sm"
+          className={hide ? 'd-none' : 'btn-custom'}
+          {...rest}
+          {...overlayProps}
+        >
+          <FontAwesomeIcon icon="trash-alt" />
+        </Button>
+      )}
+    </OverlayTrigger>
+  );
 }
 
 export default DeleteButton;
