fbb12fd7011a771ae354b85144c77978dea95866
DSoLetsDev <DSO@NC055498>
2021-09-22 13:38:01 -0700

    HETS 1331 fix for delete button popover not working when clicking with a trackpad

--- client/src/js/components/DeleteButton.jsx
+++ client/src/js/components/DeleteButton.jsx
@@ -1,53 +1,7 @@
-// import PropTypes from 'prop-types';
-// import React from 'react';
-// import { Button, OverlayTrigger } from 'react-bootstrap';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import _ from 'lodash';
-
-// import Confirm from '../components/Confirm.jsx';
-
-// class DeleteButton extends React.Component {
-//   static propTypes = {
-//     onConfirm: PropTypes.func.isRequired,
-//     name: PropTypes.string,
-//     hide: PropTypes.bool,
-//   };
-
-//   render() {
-//     var props = _.omit(this.props, 'onConfirm', 'hide', 'name');
-
-//     return (
-//       <OverlayTrigger
-//         placement="top"
-//         trigger={'click'}
-//         rootClose
-//         overlay={<Confirm onConfirm={this.props.onConfirm} />}
-//         transition={false} //removes findDOMNode warning for transitions
-//         delay={100} //required so the onConfirm function is fired on the Confirm overlay before closing. If transition={true} this isn't needed.
-//       >
-//         {(overlayProps) => (
-//           <Button
-//             title={`Delete ${this.props.name}`}
-//             size="sm"
-//             className={this.props.hide ? 'd-none' : 'btn-custom'}
-//             {...props}
-//             {...overlayProps}
-//           >
-//             <FontAwesomeIcon icon="trash-alt" />
-//           </Button>
-//         )}
-//       </OverlayTrigger>
-//     );
-//   }
-// }
-
-// export default DeleteButton;
-
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Button, OverlayTrigger } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import _ from 'lodash';
 
 import Confirm from '../components/Confirm.jsx';
 
