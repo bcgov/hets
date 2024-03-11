791763924115ffe107e9a5cd66c0728e8d621dcf
bcgov-brwang <bcgov.brwang@gmail.com>
2024-03-11 10:49:11 -0700

    TH-112626: check if the equipment is archived or approved when the user wants to to bring back the equipment from archived to approved

--- Server/HetsData/Helpers/SeniorityListHelper.cs
+++ Server/HetsData/Helpers/SeniorityListHelper.cs
@@ -203,8 +203,18 @@ namespace HetsData.Helpers
                 }
 
                 //reselect with data that include local changes
+                //var dataToProcess = dataFromDb
+                //    .Where(x => x.EquipmentStatusType != null && x.EquipmentStatusType.EquipmentStatusTypeCode == HetEquipment.StatusApproved &&
+                //                x.LocalAreaId == localAreaId &&
+                //                x.DistrictEquipmentTypeId == districtEquipmentTypeId)
+                //    .OrderByDescending(x => x.Seniority)
+                //    .ThenBy(x => x.ReceivedDate)
+                //    .ThenBy(x => x.EquipmentCode)
+                //    .ToList();
+
+                // TH-112626: check if the equipment is archived or approved when the user wants to to bring back the equipment from archived to approved 
                 var dataToProcess = dataFromDb
-                    .Where(x => x.EquipmentStatusType != null && x.EquipmentStatusType.EquipmentStatusTypeCode == HetEquipment.StatusApproved &&
+                    .Where(x => x.EquipmentStatusType != null && (x.EquipmentStatusType.EquipmentStatusTypeCode == HetEquipment.StatusApproved || x.EquipmentStatusType.EquipmentStatusTypeCode == HetEquipment.StatusArchived) &&
                                 x.LocalAreaId == localAreaId &&
                                 x.DistrictEquipmentTypeId == districtEquipmentTypeId)
                     .OrderByDescending(x => x.Seniority)
