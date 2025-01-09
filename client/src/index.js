623cad2c17d9dc7f7635d8ddf5b3b363fa00f7af
Abhishek Dere <abhishek.dere@gov.bc.ca>
2025-01-08 16:06:37 -0800

    Revert "Updated  version of the dependencies"
    This reverts commit 2a61985773239479a7560be75575e12fcd5db360.

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './js/App';
 import reportWebVitals from './reportWebVitals';
 
@@ -11,17 +11,15 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './sass/main.scss';
 
 Keycloak.init(() => {
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-
-  root.render(
+  ReactDOM.render(
     <Provider store={store}>
       <React.StrictMode>
         <App />
       </React.StrictMode>
-    </Provider>
+    </Provider>,
+    document.getElementById('root')
   );
 });
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- client/src/js/views/Home.jsx
+++ client/src/js/views/Home.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
-import React, { useEffect } from 'react';
-
+import React from 'react';
+import { connect } from 'react-redux';
 import { Row, Col, Button } from 'react-bootstrap';
 import _ from 'lodash';
 
@@ -10,99 +10,131 @@ import * as Constant from '../constants';
 
 import PageHeader from '../components/ui/PageHeader.jsx';
 import SubHeader from '../components/ui/SubHeader.jsx';
-import { useDispatch, useSelector } from 'react-redux';
 
-const Home = ({ history }) => {
-  const dispatch = useDispatch();
-  const currentUser = useSelector((state) => state.user);
-  const searchSummaryCounts = useSelector((state) => state.lookups.searchSummaryCounts);
-
-  useEffect(() => {
-    
-    fetch();
-  }, []); 
-
-  const fetch = () => {
-    dispatch(Api.getSearchSummaryCounts());
+class Home extends React.Component {
+  static propTypes = {
+    currentUser: PropTypes.object,
+    searchSummaryCounts: PropTypes.object,
+    history: PropTypes.object,
   };
 
-  const goToUnapprovedOwners = () => {
-    const unapprovedStatus = Constant.OWNER_STATUS_CODE_PENDING;
+  componentDidMount() {
+    this.fetch();
+  }
 
-    dispatch({ type: Action.UPDATE_OWNERS_SEARCH, owners: { statusCode: unapprovedStatus } });
-    dispatch(Api.searchOwners({ status: unapprovedStatus }));
-
-    history.push({ pathname: Constant.OWNERS_PATHNAME });
+  fetch = () => {
+    this.props.dispatch(Api.getSearchSummaryCounts());
   };
 
-  const goToUnapprovedEquipment = () => {
-    const unapprovedStatus = Constant.EQUIPMENT_STATUS_CODE_PENDING;
+  goToUnapprovedOwners = () => {
+    var unapprovedStatus = Constant.OWNER_STATUS_CODE_PENDING;
 
-    dispatch({ type: Action.UPDATE_EQUIPMENT_LIST_SEARCH, equipmentList: { statusCode: unapprovedStatus } });
-    dispatch(Api.searchEquipmentList({ status: unapprovedStatus }));
-
-    history.push({ pathname: Constant.EQUIPMENT_PATHNAME });
-  };
-
-  const goToHiredEquipment = () => {
-    dispatch({
-      type: Action.UPDATE_EQUIPMENT_LIST_SEARCH,
-      equipmentList: { statusCode: Constant.EQUIPMENT_STATUS_CODE_APPROVED, hired: true },
+    // update search parameters
+    this.props.dispatch({
+      type: Action.UPDATE_OWNERS_SEARCH,
+      owners: { statusCode: unapprovedStatus },
     });
-    dispatch(Api.searchEquipmentList({
+
+    // perform search
+    this.props.dispatch(Api.searchOwners({ status: unapprovedStatus }));
+
+    // navigate to search page
+    this.props.history.push({ pathname: Constant.OWNERS_PATHNAME });
+  };
+
+  goToUnapprovedEquipment = () => {
+    var unapprovedStatus = Constant.EQUIPMENT_STATUS_CODE_PENDING;
+
+    // update search parameters
+    this.props.dispatch({
+      type: Action.UPDATE_EQUIPMENT_LIST_SEARCH,
+      equipmentList: { statusCode: Constant.EQUIPMENT_STATUS_CODE_PENDING },
+    });
+
+    // perform search
+    this.props.dispatch(Api.searchEquipmentList({ status: unapprovedStatus }));
+
+    // navigate to search page
+    this.props.history.push({ pathname: Constant.EQUIPMENT_PATHNAME });
+  };
+
+  goToHiredEquipment = () => {
+    // update search parameters
+    this.props.dispatch({
+      type: Action.UPDATE_EQUIPMENT_LIST_SEARCH,
+      equipmentList: {
+        statusCode: Constant.EQUIPMENT_STATUS_CODE_APPROVED,
+        hired: true,
+      },
+    });
+
+    // perform search
+    this.props.dispatch(Api.searchEquipmentList({
       status: Constant.EQUIPMENT_STATUS_CODE_APPROVED,
       hired: true,
     }));
 
-    history.push({ pathname: Constant.EQUIPMENT_PATHNAME });
+    // navigate to search page
+    this.props.history.push({ pathname: Constant.EQUIPMENT_PATHNAME });
   };
 
-  const goToBlockedRotationLists = () => {
-    dispatch({
+  goToBlockedRotationLists = () => {
+    // update search parameters
+    this.props.dispatch({
       type: Action.UPDATE_RENTAL_REQUESTS_SEARCH,
-      rentalRequests: { statusCode: Constant.RENTAL_REQUEST_STATUS_CODE_IN_PROGRESS },
+      rentalRequests: {
+        statusCode: Constant.RENTAL_REQUEST_STATUS_CODE_IN_PROGRESS,
+      },
     });
-    dispatch(Api.searchRentalRequests({
+
+    // perform search
+    this.props.dispatch(Api.searchRentalRequests({
       status: Constant.RENTAL_REQUEST_STATUS_CODE_IN_PROGRESS,
     }));
 
-    history.push({ pathname: Constant.RENTAL_REQUESTS_PATHNAME });
+    // navigate to search page
+    this.props.history.push({ pathname: Constant.RENTAL_REQUESTS_PATHNAME });
   };
 
-  const counts = searchSummaryCounts;
+  render() {
+    var counts = this.props.searchSummaryCounts;
 
-  return (
-    <div id="home">
-      <PageHeader>
-        {currentUser.fullName}
-        <br />
-        {currentUser.districtName} District
-      </PageHeader>
-      <div className="well">
-        <SubHeader title="Summary" />
-        <Row>
-          <Col md={12} className="btn-container">
-            <Button variant="primary" className="btn-custom" onClick={goToUnapprovedOwners}>
-              Unapproved owners {!_.isEmpty(counts) && `(${counts.unapprovedOwners})`}
-            </Button>
-            <Button variant="primary" className="btn-custom" onClick={goToUnapprovedEquipment}>
-              Unapproved equipment {!_.isEmpty(counts) && `(${counts.unapprovedEquipment})`}
-            </Button>
-            <Button variant="primary" className="btn-custom" onClick={goToHiredEquipment}>
-              Currently hired equipment {!_.isEmpty(counts) && `(${counts.hiredEquipment})`}
-            </Button>
-            <Button variant="primary" className="btn-custom" onClick={goToBlockedRotationLists}>
-              Blocked rotation lists {!_.isEmpty(counts) && `(${counts.inProgressRentalRequests})`}
-            </Button>
-          </Col>
-        </Row>
+    return (
+      <div id="home">
+        <PageHeader>
+          {this.props.currentUser.fullName}
+          <br />
+          {this.props.currentUser.districtName} District
+        </PageHeader>
+        <div className="well">
+          <SubHeader title="Summary" />
+          <Row>
+            <Col md={12} className="btn-container">
+              <Button variant="primary" className="btn-custom" onClick={this.goToUnapprovedOwners}>
+                Unapproved owners {!_.isEmpty(counts) && `(${counts.unapprovedOwners})`}
+              </Button>
+              <Button variant="primary" className="btn-custom" onClick={this.goToUnapprovedEquipment}>
+                Unapproved equipment {!_.isEmpty(counts) && `(${counts.unapprovedEquipment})`}
+              </Button>
+              <Button variant="primary" className="btn-custom" onClick={this.goToHiredEquipment}>
+                Currently hired equipment {!_.isEmpty(counts) && `(${counts.hiredEquipment})`}
+              </Button>
+              <Button variant="primary" className="btn-custom" onClick={this.goToBlockedRotationLists}>
+                Blocked rotation lists {!_.isEmpty(counts) && `(${counts.inProgressRentalRequests})`}
+              </Button>
+            </Col>
+          </Row>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
-Home.propTypes = {
-  history: PropTypes.object,
-};
+const mapStateToProps = (state) => ({
+  currentUser: state.user,
+  searchSummaryCounts: state.lookups.searchSummaryCounts,
+});
 
-export default Home;
+const mapDispatchToProps = (dispatch) => ({ dispatch });
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
