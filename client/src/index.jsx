79605cfbcc80f21746f7dce7f008ff17cb608481
Ionwyn Sean <ionwynsean@gmail.com>
2026-08-05 11:25:33 -0700

    Move index to source as jsx
    Signed-off-by: Ionwyn Sean <ionwynsean@gmail.com>

---
+++ client/src/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './js/App';
+import reportWebVitals from './reportWebVitals';
+
+import * as Keycloak from './js/Keycloak';
+import { Provider } from 'react-redux';
+import { store } from './js/store';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './sass/main.scss';
+
+Keycloak.init(() => {
+  const root = createRoot(document.getElementById('root'));
+  root.render(
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
+  );
+});
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
