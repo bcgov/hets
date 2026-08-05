bcc4557e14aac25f0624815284ed856165bed53d
Ionwyn Sean <ionwynsean@gmail.com>
2026-08-05 11:26:24 -0700

    Setup web builder
    Signed-off-by: Ionwyn Sean <ionwynsean@gmail.com>

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -2,7 +2,10 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import { vi } from 'vitest';
+import '@testing-library/jest-dom/vitest';
 import { registerFaIcons } from './registerFaIcons';
+
+globalThis.jest = vi;
 
 registerFaIcons();
