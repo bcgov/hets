b76961cde6c42e58ef0e273430a47e112f1815e5
Ionwyn Sean <ionwynsean@gmail.com>
2026-08-13 09:44:16 -0700

    [HETS-1366] Add test for Date Utility
    Signed-off-by: Ionwyn Sean <ionwynsean@gmail.com>

--- Server/HetsCommon.Tests/DateUtilsTests.cs
+++ Server/HetsCommon.Tests/DateUtilsTests.cs
@@ -1,3 +1,4 @@
+using System;
 using HetsCommon;
 using Xunit;
 
@@ -5,6 +6,38 @@ namespace HetsCommon.Tests;
 
 public class DateUtilsTests
 {
+    [Theory]
+    [InlineData(2026, 3, 9, 0, 0, 0, 2026, 3, 8, 17, 0, 0)]
+    [InlineData(2026, 11, 1, 9, 0, 0, 2026, 11, 1, 2, 0, 0)]
+    [InlineData(2026, 12, 15, 12, 0, 0, 2026, 12, 15, 5, 0, 0)]
+    public void ConvertUtcToPacificTime_UsesPermanentUtcMinusSevenAfterEffectiveDate(
+        int utcYear, int utcMonth, int utcDay, int utcHour, int utcMinute, int utcSecond,
+        int expectedYear, int expectedMonth, int expectedDay, int expectedHour, int expectedMinute, int expectedSecond)
+    {
+        DateTime utc = new(utcYear, utcMonth, utcDay, utcHour, utcMinute, utcSecond, DateTimeKind.Utc);
+
+        DateTime result = DateUtils.ConvertUtcToPacificTime(utc);
+
+        Assert.Equal(new DateTime(expectedYear, expectedMonth, expectedDay, expectedHour, expectedMinute, expectedSecond), result);
+        Assert.Equal(DateTimeKind.Unspecified, result.Kind);
+    }
+
+    [Theory]
+    [InlineData(2026, 3, 9, 0, 0, 0, 2026, 3, 9, 7, 0, 0)]
+    [InlineData(2026, 11, 1, 2, 0, 0, 2026, 11, 1, 9, 0, 0)]
+    [InlineData(2026, 12, 15, 5, 0, 0, 2026, 12, 15, 12, 0, 0)]
+    public void ConvertPacificToUtcTime_UsesPermanentUtcMinusSevenAfterEffectiveDate(
+        int localYear, int localMonth, int localDay, int localHour, int localMinute, int localSecond,
+        int expectedYear, int expectedMonth, int expectedDay, int expectedHour, int expectedMinute, int expectedSecond)
+    {
+        DateTime local = new(localYear, localMonth, localDay, localHour, localMinute, localSecond, DateTimeKind.Unspecified);
+
+        DateTime result = DateUtils.ConvertPacificToUtcTime(local);
+
+        Assert.Equal(new DateTime(expectedYear, expectedMonth, expectedDay, expectedHour, expectedMinute, expectedSecond, DateTimeKind.Utc), result);
+        Assert.Equal(DateTimeKind.Utc, result.Kind);
+    }
+
     [Theory]
     [InlineData(DateTimeKind.Utc)]
     [InlineData(DateTimeKind.Local)]
